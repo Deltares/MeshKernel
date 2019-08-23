9b89579c27f373902121c907796414b019a96c05
carniato <carniato@deltares.nl>
2019-08-23 16:30:09 +0000

    Add google tests

---
+++ src/tests/GridGeomTest.cpp
@@ -0,0 +1,14 @@
+#include "GridGeomTest.hpp"
+
+TEST (OneQuad,OneQuad) 
+{
+	//check values
+	EXPECT_EQ(0, 0);
+	EXPECT_EQ(10, 10);
+	EXPECT_DOUBLE_EQ(0.0, 0.0);
+}
+
+
+
+
+
---
+++ src/tests/GridGeomTest.hpp
@@ -0,0 +1,9 @@
+#ifndef GRIDGEOM_TESTS_HPP
+#define GRIDGEOM_TESTS_HPP
+
+#include "../CartesianPoint.cpp"
+#include "../SphericalPoint.cpp"
+#include "../Mesh.cpp"
+#include <gtest/gtest.h>
+
+#endif
