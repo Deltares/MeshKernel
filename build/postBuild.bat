5f62ee6b77ac8c092c16c4cb56006537beb9a0ca
carniato <carniato@deltares.nl>
2019-10-18 11:25:35 +0000

    + account for state in prepare orthogonalization + backup original net for correct projection + store nodes with problems for later visualization + add post-build step for debugging

--- build/postBuild.bat
+++ build/postBuild.bat
@@ -1,3 +1,5 @@
-copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\src\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
-copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
-copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.pdb" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.pdb"
+rem copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\src\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
+rem copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
+rem copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.pdb" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.pdb"
+copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Release\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\src\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
+copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Release\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
