aa048cae5f253262cd206b296938897a0a7e73a3
carniato <carniato@deltares.nl>
2019-10-18 08:27:37 +0000

    Add postbuild bat

---
+++ build/postBuild.bat
@@ -0,0 +1,3 @@
+copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\src\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
+copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.dll" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
+copy /y "D:\LUCA\ENGINES\GridGeom++\gridgeom++\build\Debug\gridgeomStateful_dll.pdb" "D:\LUCA\ENGINES\GridGeom++\gridgeom++\GridEditor\bin\Debug\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.pdb"
