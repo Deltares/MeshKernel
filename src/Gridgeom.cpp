f7cace784e6b614340a565346c6a80fca83b196f
carniato <carniato@deltares.nl>
2020-01-07 13:46:47 +0000

    First implementation of Splines

--- src/Gridgeom.cpp
+++ src/Gridgeom.cpp
@@ -3,9 +3,11 @@
 #include "Gridgeom.hpp"
 #include "Mesh.hpp"
 #include "Orthogonalization.hpp"
+#include "Splines.hpp"
 
-static std::vector<GridGeom::Mesh> meshInstances{};
-static std::map<int,GridGeom::Orthogonalization> orthogonalizationInstances;
+static std::vector<GridGeom::Mesh> meshInstances;
+static std::map<int, GridGeom::Orthogonalization> orthogonalizationInstances;
+static std::map<int, GridGeom::Splines> splinesInstances;
 
 namespace GridGeomApi
 {
@@ -13,7 +15,7 @@ namespace GridGeomApi
     {
         int instanceSize = meshInstances.size();
         meshInstances.resize(instanceSize + 1);
-        gridStateId = instanceSize; 
+        gridStateId = instanceSize;
         return 0;
     };
 
@@ -65,20 +67,20 @@ namespace GridGeomApi
 
     GRIDGEOM_API int ggeo_get_mesh(int& gridStateId, MeshGeometryDimensions& meshGeometryDimensions, MeshGeometry& meshGeometry)
     {
-        
+
         meshInstances[gridStateId].SetFlatCopies();
 
         meshGeometry.nodex = &meshInstances[gridStateId].m_nodex[0];
         meshGeometry.nodey = &meshInstances[gridStateId].m_nodey[0];
         meshGeometry.nodez = &meshInstances[gridStateId].m_nodez[0];
-        meshGeometry.edge_nodes = &meshInstances[gridStateId].m_edgeNodes[0];            
+        meshGeometry.edge_nodes = &meshInstances[gridStateId].m_edgeNodes[0];
 
-        if (meshInstances[gridStateId].m_nodex.size()==1)
+        if (meshInstances[gridStateId].m_nodex.size() == 1)
         {
             meshGeometryDimensions.numnode = 0;
             meshGeometryDimensions.numedge = 0;
             meshGeometryDimensions.numface = 0;
-            meshGeometryDimensions.maxnumfacenodes = 4; 
+            meshGeometryDimensions.maxnumfacenodes = 4;
         }
         else
         {
@@ -98,7 +100,7 @@ namespace GridGeomApi
 
         // build enclosing polygon
         std::vector<GridGeom::Point> polygon(geometryListNativePolygon.numberOfCoordinates);
-        for (int i=0; i<geometryListNativePolygon.numberOfCoordinates; i++)
+        for (int i = 0; i < geometryListNativePolygon.numberOfCoordinates; i++)
         {
             polygon[i].x = geometryListNativePolygon.xCoordinates[i];
             polygon[i].y = geometryListNativePolygon.yCoordinates[i];
@@ -106,7 +108,7 @@ namespace GridGeomApi
 
         // build land boundary
         std::vector<GridGeom::Point> landBoundaries(geometryListNativeLandBoundaries.numberOfCoordinates);
-        for (int i = 0; i<geometryListNativeLandBoundaries.numberOfCoordinates; i++)
+        for (int i = 0; i < geometryListNativeLandBoundaries.numberOfCoordinates; i++)
         {
             landBoundaries[i].x = geometryListNativeLandBoundaries.xCoordinates[i];
             landBoundaries[i].y = geometryListNativeLandBoundaries.yCoordinates[i];
@@ -133,7 +135,7 @@ namespace GridGeomApi
     {
         // build enclosing polygon
         std::vector<GridGeom::Point> polygon(geometryListNativePolygon.numberOfCoordinates);
-        for (int i = 0; i<geometryListNativePolygon.numberOfCoordinates; i++)
+        for (int i = 0; i < geometryListNativePolygon.numberOfCoordinates; i++)
         {
             polygon[i].x = geometryListNativePolygon.xCoordinates[i];
             polygon[i].y = geometryListNativePolygon.yCoordinates[i];
@@ -141,7 +143,7 @@ namespace GridGeomApi
 
         // build land boundary
         std::vector<GridGeom::Point> landBoundaries(geometryListNativeLandBoundaries.numberOfCoordinates);
-        for (int i = 0; i<geometryListNativeLandBoundaries.numberOfCoordinates; i++)
+        for (int i = 0; i < geometryListNativeLandBoundaries.numberOfCoordinates; i++)
         {
             landBoundaries[i].x = geometryListNativeLandBoundaries.xCoordinates[i];
             landBoundaries[i].y = geometryListNativeLandBoundaries.yCoordinates[i];
@@ -178,7 +180,7 @@ namespace GridGeomApi
     GRIDGEOM_API int ggeo_orthogonalize_delete(int& gridStateId)
     {
         const int returnValue = orthogonalizationInstances.erase(gridStateId);
-        return returnValue == 1 ?  0 : 1;
+        return returnValue == 1 ? 0 : 1;
     }
 
     GRIDGEOM_API int ggeo_get_orthogonality(int& gridStateId, GeometryListNative& geometryList)
@@ -191,6 +193,102 @@ namespace GridGeomApi
     {
         const bool status = orthogonalizationInstances[gridStateId].GetSmoothness(meshInstances[gridStateId], geometryList.zCoordinates);
         return status == true ? 0 : 1;
+    }
+
+    GRIDGEOM_API int ggeo_get_splines(GeometryListNative& geometryListIn, GeometryListNative& geometry_list_out, int& number_of_points_between_vertices)
+    {
+
+        std::vector<GridGeom::Point> splines(geometryListIn.numberOfCoordinates);
+        for (int i = 0; i < geometryListIn.numberOfCoordinates; i++)
+        {
+            splines[i].x = geometryListIn.xCoordinates[i];
+            splines[i].y = geometryListIn.yCoordinates[i];
+        }
+
+        std::vector<std::vector<int>> indexes(geometryListIn.numberOfCoordinates, std::vector<int>(2));
+        int numSplines = FindIndexes(splines, GridGeom::doubleMissingValue, indexes);
+        std::vector<double> xCoordinatesDerivatives(geometryListIn.numberOfCoordinates);
+        std::vector<double> yCoordinatesDerivatives(geometryListIn.numberOfCoordinates);
+
+        // count the total number of output points
+        geometry_list_out.numberOfCoordinates = 0;
+        for (int s = 0; s < numSplines; s++)
+        {
+            // points for rendering
+            geometry_list_out.numberOfCoordinates = geometry_list_out.numberOfCoordinates + number_of_points_between_vertices * (indexes[s][1] - indexes[s][0]);
+            // add separator
+            geometry_list_out.numberOfCoordinates += 1;
+        }
+
+        if (geometry_list_out.xCoordinates == nullptr || geometry_list_out.yCoordinates == nullptr)
+        {
+            return -1;
+        }
+
+        int index = 0;
+        for (int s = 0; s < numSplines; s++)
+        {
+            int numNodes = indexes[s][1] - indexes[s][0];
+            std::vector<double> xCoordinates(geometryListIn.xCoordinates + indexes[s][0], geometryListIn.xCoordinates + indexes[s][1]);
+            std::vector<double> yCoordinates(geometryListIn.yCoordinates + indexes[s][0], geometryListIn.yCoordinates + indexes[s][1]);
+
+            GridGeom::Splines::SecondOrderDerivative(xCoordinates, xCoordinatesDerivatives);
+            GridGeom::Splines::SecondOrderDerivative(xCoordinates, yCoordinatesDerivatives);
+
+            for (int n = 0; n < numNodes; n++)
+            {
+                for (int p = 0; p < number_of_points_between_vertices; p++)
+                {
+
+                    double pointAdimensionalCoordinate = n + p / number_of_points_between_vertices;
+                    double xPointCoordinate;
+                    GridGeom::Splines::Interpolate(xCoordinates, xCoordinatesDerivatives, pointAdimensionalCoordinate, xPointCoordinate);
+                    geometry_list_out.xCoordinates[index] = xPointCoordinate;
+
+                    double yPointCoordinate;
+                    GridGeom::Splines::Interpolate(yCoordinates, yCoordinatesDerivatives, pointAdimensionalCoordinate, yPointCoordinate);
+                    geometry_list_out.yCoordinates[index] = yPointCoordinate;
+
+                    geometry_list_out.zCoordinates[index] = GridGeom::doubleMissingValue;
+                    index++;
+                }
+            }
+
+            geometry_list_out.xCoordinates[index] = GridGeom::doubleMissingValue;
+            geometry_list_out.yCoordinates[index] = GridGeom::doubleMissingValue;
+            geometry_list_out.zCoordinates[index] = GridGeom::doubleMissingValue;
+            index++;
+        }
+        return 0;
+    }
+
+    GRIDGEOM_API int ggeo_set_splines(int& gridStateId, GeometryListNative& geometryListIn)
+    {
+        std::vector<GridGeom::Point> splines(geometryListIn.numberOfCoordinates);
+        for (int i = 0; i < geometryListIn.numberOfCoordinates; i++)
+        {
+            splines[i].x = geometryListIn.xCoordinates[i];
+            splines[i].y = geometryListIn.yCoordinates[i];
+        }
+
+        // use the default constructor, no values are allocated
+        if (splinesInstances.count(gridStateId) ==0)
+        {
+            splinesInstances[gridStateId] = GridGeom::Splines();
+        }
+
+        std::vector<std::vector<int>> indexes(geometryListIn.numberOfCoordinates, std::vector<int>(2));
+        int numSplines = FindIndexes(splines, GridGeom::doubleMissingValue, indexes);
+        for (int s = 0; s < numSplines; s++)
+        {
+            for (int p = indexes[s][0]; p <= indexes[s][1]; p++)
+            {
+                const bool status = splinesInstances[gridStateId].Set(s, splines[p]);
+                if (!status) return -1;
+            }
+        }
+
+        return 0;
     }
 
 }
