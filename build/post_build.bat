a84fc65eceeb0ae3cfbb4e2fab31d2a7a0a8e517
carniato <carniato@deltares.nl>
2020-01-08 10:35:06 +0000

    copy debug dll

--- build/post_build.bat
+++ build/post_build.bat
@@ -1,4 +1,4 @@
-set version=Release
+set version=Debug
 copy /y ".\%version%\gridgeomStateful_dll.dll" "..\GridEditor\src\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
 copy /y ".\%version%\gridgeomStateful_dll.pdb" "..\GridEditor\bin\Release\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.pdb"
 copy /y ".\%version%\gridgeomStateful_dll.dll" "..\GridEditor\bin\Release\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
