3a1251e37cb08009eb807ee6499c05f42f078eae
carniato <carniato@deltares.nl>
2020-01-28 16:58:46 +0000

    Unit tested ComputeGridHeights

--- src/tests/SplineTest.cpp
+++ src/tests/SplineTest.cpp
@@ -18,10 +18,10 @@ TEST(SplineTests, SetSpline)
     bool success = splines.AddSpline(splineNodes);
     ASSERT_TRUE(success);
 
-    EXPECT_EQ(splines.m_numSplines, 1);
-    EXPECT_EQ(splines.m_splineCornerPoints[0].size(), splineNodes.size());
-    EXPECT_EQ(splines.m_numAllocatedSplines, 5);
-    EXPECT_EQ(splines.m_numAllocatedSplineNodes[0], 10);
+    ASSERT_EQ(1, splines.m_numSplines);
+    ASSERT_EQ(10, splines.m_splineCornerPoints[0].size());
+    ASSERT_EQ(5, splines.m_numAllocatedSplines);
+    ASSERT_EQ(10, splines.m_numAllocatedSplineNodes[0]);
 }
 
 TEST(SplineTests, CubicSplineInterpolation)
@@ -119,8 +119,7 @@ TEST(SplineTests, ComputeSplinesProperties)
     ASSERT_TRUE(success);
     success = splines.MakeAllGridLines(true);
     ASSERT_TRUE(success);
-    success = splines.OrthogonalCurvilinearMeshFromSplines();
-
+    ASSERT_EQ(7, splines.m_numGridLines);
 
     const double tolerance = 1e-3;
     ASSERT_NEAR(253.52971595547601, splines.m_maximumGridHeights[0], tolerance);
@@ -150,47 +149,6 @@ TEST(SplineTests, ComputeSplinesProperties)
     ASSERT_NEAR(1.09068327269294, splines.m_gridLineDimensionalCoordinates[5], tolerance);
     ASSERT_NEAR(4.214936859441928e-14, splines.m_gridLineDimensionalCoordinates[6], tolerance);
 }
-
-TEST(SplineTests, OrthogonalCurvilinearMeshFromSplines)
-{
-    std::vector<GridGeom::Point> firstSpline;
-    firstSpline.push_back(GridGeom::Point{ 152.001571655273, 86.6264953613281 });
-    firstSpline.push_back(GridGeom::Point{ 374.752960205078, 336.378997802734 });
-    firstSpline.push_back(GridGeom::Point{ 850.255920410156, 499.130676269531 });
-
-    GridGeom::Polygons polygon;
-    GridGeom::Splines splines(GridGeom::Projections::cartesian, polygon);
-    bool success = splines.AddSpline(firstSpline);
-    ASSERT_TRUE(success);
-
-    std::vector<GridGeom::Point> secondSpline;
-    secondSpline.push_back(GridGeom::Point{ 72.5010681152344,391.129577636719 });
-    secondSpline.push_back(GridGeom::Point{ 462.503479003906, 90.3765411376953 });
-    success = splines.AddSpline(secondSpline);
-
-    success = splines.ComputeSplineProperties(false);
-    ASSERT_TRUE(success);
-    success = splines.MakeAllGridLines(true);
-    ASSERT_TRUE(success);
-    success = splines.OrthogonalCurvilinearMeshFromSplines();
-    ASSERT_TRUE(success);
-
-    ASSERT_EQ(0, splines.m_numCrossSplineLeftHeights[0][0]);
-    ASSERT_EQ(1, splines.m_numCrossSplineLeftHeights[0][1]);
-    ASSERT_EQ(0, splines.m_numCrossSplineLeftHeights[0][2]);
-    ASSERT_EQ(GridGeom::intMissingValue, splines.m_numCrossSplineLeftHeights[0][3]);
-
-    for (int s = 1; s < splines.m_numSplines; ++s)
-    {
-        for (int ss = 0; ss < splines.m_numSplines; ++ss)
-        {
-            ASSERT_EQ(GridGeom::intMissingValue, splines.m_numCrossSplineLeftHeights[s][ss]);
-        }
-    }
-
-
-}
-
 
 TEST(SplineTests, ComputeBoundingBox)
 {
@@ -231,4 +189,46 @@ TEST(SplineTests, ComputeBoundingBox)
     ASSERT_NEAR(370.339417298715, splines.m_maximumGridHeights[1], tolerance);
     ASSERT_NEAR(0.0, splines.m_maximumGridHeights[2], tolerance);
     ASSERT_NEAR(0.0, splines.m_maximumGridHeights[3], tolerance);
+}
+
+TEST(SplineTests, OrthogonalCurvilinearMeshFromSplines)
+{
+    std::vector<GridGeom::Point> firstSpline;
+    firstSpline.push_back(GridGeom::Point{ 152.001571655273, 86.6264953613281 });
+    firstSpline.push_back(GridGeom::Point{ 374.752960205078, 336.378997802734 });
+    firstSpline.push_back(GridGeom::Point{ 850.255920410156, 499.130676269531 });
+
+    GridGeom::Polygons polygon;
+    GridGeom::Splines splines(GridGeom::Projections::cartesian, polygon);
+    bool success = splines.AddSpline(firstSpline);
+    ASSERT_TRUE(success);
+
+    std::vector<GridGeom::Point> secondSpline;
+    secondSpline.push_back(GridGeom::Point{ 72.5010681152344,391.129577636719 });
+    secondSpline.push_back(GridGeom::Point{ 462.503479003906, 90.3765411376953 });
+    success = splines.AddSpline(secondSpline);
+
+    success = splines.OrthogonalCurvilinearMeshFromSplines();
+    ASSERT_TRUE(success);
+
+    ASSERT_EQ(0, splines.m_numCrossSplineLeftHeights[0][0]);
+    ASSERT_EQ(1, splines.m_numCrossSplineLeftHeights[0][1]);
+    ASSERT_EQ(0, splines.m_numCrossSplineLeftHeights[0][2]);
+    ASSERT_EQ(GridGeom::intMissingValue, splines.m_numCrossSplineLeftHeights[0][3]);
+
+    for (int s = 1; s < splines.m_numSplines; ++s)
+    {
+        for (int ss = 0; ss < splines.m_numSplines; ++ss)
+        {
+            ASSERT_EQ(GridGeom::intMissingValue, splines.m_numCrossSplineLeftHeights[s][ss]);
+        }
+    }
+
+    const double tolerance = 1e-3;
+    ASSERT_NEAR(238.968273342307, splines.m_gridHeights[0][0], tolerance);
+    ASSERT_NEAR(238.968273342307, splines.m_gridHeights[0][1], tolerance);
+    ASSERT_NEAR(GridGeom::doubleMissingValue, splines.m_maximumGridHeights[2], tolerance);
+    ASSERT_NEAR(GridGeom::doubleMissingValue, splines.m_maximumGridHeights[3], tolerance);
+    ASSERT_NEAR(253.529715955476, splines.m_maximumGridHeights[4], tolerance);
+    ASSERT_NEAR(253.529715955476, splines.m_maximumGridHeights[5], tolerance);
 }
