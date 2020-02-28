f569271c092f4333d8db15546c8a58f46a6808d0
carniato <carniato@deltares.nl>
2020-02-28 15:45:02 +0000

    successfully linked triangulation

--- src/tests/GridGeomTest.hpp
+++ src/tests/GridGeomTest.hpp
@@ -4,4 +4,10 @@
 #include "../Entities.hpp"
 #include "../Operations.cpp"
 #include "../Polygons.hpp"
+#include "../LandBoundaries.cpp"
+#include "../Constants.cpp"
+#include "../Orthogonalization.cpp"
+#include "../GeometryListNative.hpp"
+#include "../Splines.hpp"
+
 #include <gtest/gtest.h>
