0601addcebb2d43387d3d302870f83e081b40730
carniato <carniato@deltares.nl>
2020-04-17 15:18:20 +0000

    Created ComputePolygonCircumcenter and refactor

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -17,7 +17,7 @@ namespace GridGeom
     static constexpr double degrad_hp = M_PI / 180.0; // conversion factor from degrees to radians(pi / 180)
     static constexpr double raddeg_hp = 180.0 / M_PI; // conversion factor from radians to degrees(180 / pi)
     static constexpr double earth_radius = 6378137.0; // earth radius(m)
-    static constexpr double dtol_pole = 0.0001;       // pole tolerance in degrees
+    static constexpr double absLatitudeAtPoles = 0.0001;       // pole tolerance in degrees
 
     //mesh constants
     static constexpr double minimumDeltaCoordinate = 1e-14;
