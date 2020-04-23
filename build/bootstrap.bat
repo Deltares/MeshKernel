cd282161be8a1096d3ae10b1db3054715153eabb
carniato <carniato@deltares.nl>
2020-04-23 11:08:11 +0000

    minor changes

--- build/bootstrap.bat
+++ build/bootstrap.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake .. -G"Visual Studio 16 2019" -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_CXX_FLAGS_RELEASE="-g"
+cmake .. -G"Visual Studio 16 2019"
 pause
--- build/post_build.bat
+++ build/post_build.bat
@@ -3,3 +3,5 @@ set guiVersion=Debug
 copy /y ".\%libVersion%\gridgeomStateful_dll.dll" "..\GridEditor\src\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
 copy /y ".\%libVersion%\gridgeomStateful_dll.pdb" "..\GridEditor\bin\%guiVersion%\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.pdb"
 copy /y ".\%libVersion%\gridgeomStateful_dll.dll" "..\GridEditor\bin\%guiVersion%\plugins\DeltaShell.Plugins.GridEditor\Lib\gridgeomStateful_dll.dll"
+
+
