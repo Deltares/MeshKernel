c46c599ac744a5f8f2effdb37a57813ace8496a1
lucacarniato <carniato.luca@gmail.com>
2020-10-06 14:44:23 +0200

    GRIDEDIT-175: solve issue when refining: circumcenters calculated wrongly because DotProduct called with wrong arguments and if (mappedEdge > 0)  should have been if (mappedEdge >= 0)

--- build/post_build.bat
+++ build/post_build.bat
@@ -1,5 +1,5 @@
-set libVersion=Release
+set libVersion=Debug
 set guiVersion=%libVersion%
-copy /y ".\%libVersion%\lib\MeshKernel.dll" "C:\engines\Grid_Editor_plugin\src\DeltaShell.Plugins.GridEditor\Lib\MeshKernel.dll"
-copy /y ".\%libVersion%\lib\MeshKernel.pdb" "C:\engines\Grid_Editor_plugin\bin\%guiVersion%\plugins\DeltaShell.Plugins.GridEditor\Lib\MeshKernel.pdb"
-copy /y ".\%libVersion%\lib\MeshKernel.dll" "C:\engines\Grid_Editor_plugin\bin\%guiVersion%\plugins\DeltaShell.Plugins.GridEditor\Lib\MeshKernel.dll"
+copy /y ".\%libVersion%\lib\MeshKernel.dll" "D:\ENGINES\DeltaShell\Grid_Editor_plugin\packages\Deltares.MeshKernel.1.0.0.36\native\Lib"
+copy /y ".\%libVersion%\lib\MeshKernel.pdb" "D:\ENGINES\DeltaShell\Grid_Editor_plugin\bin\%guiVersion%\plugins\DeltaShell.Plugins.GridEditor\Lib\MeshKernel.pdb"
+copy /y ".\%libVersion%\lib\MeshKernel.dll" "D:\ENGINES\DeltaShell\Grid_Editor_plugin\bin\%guiVersion%\plugins\DeltaShell.Plugins.GridEditor\Lib\MeshKernel.dll"
