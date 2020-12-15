f6d0ebad6a30415791fd297b4ee775fb78aa024f
lucacarniato <carniato.luca@gmail.com>
2020-12-15 18:43:01 +0100

    GRIDEDIT-269: waiting c++20...

--- tests/api/ApiTest.cpp
+++ tests/api/ApiTest.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <MeshKernel/GeometryList.hpp>
 #include <MeshKernel/MeshGeometry.hpp>
 #include <MeshKernel/MeshGeometryDimensions.hpp>
 #include <MeshKernel/MeshKernel.hpp>
@@ -16,11 +17,11 @@ public:
         ASSERT_EQ(0, meshKernelId);
 
         // Set-up new mesh
-        auto [meshGeometry, meshGeometryDimensions] = MakeRectangularMeshForApiTesting(n, m, delta);
-        auto errorCode = mkernel_set_state(meshKernelId, meshGeometryDimensions, meshGeometry, false);
+        auto mesh = MakeRectangularMeshForApiTesting(n, m, delta);
+        auto errorCode = mkernel_set_state(meshKernelId, std::get<1>(mesh), std::get<0>(mesh), false);
         ASSERT_EQ(0, errorCode);
         // Clean up client-allocated memory
-        DeleteRectangularMeshForApiTesting(meshGeometry);
+        DeleteRectangularMeshForApiTesting(std::get<0>(mesh));
     }
 
 protected:
@@ -92,4 +93,82 @@ TEST_F(ApiTest, InsertEdgeThroughApi)
     ASSERT_EQ(0, errorCode);
     ASSERT_EQ(9, meshGeometryDimensions.numnode);
     ASSERT_EQ(13, meshGeometryDimensions.numedge);
+}
+
+TEST_F(ApiTest, MergeTwoNodesThroughApi)
+{
+    // Set a new mesh in Mesh Kernel
+    MakeMesh();
+
+    // Execute
+    auto errorCode = meshkernelapi::mkernel_merge_two_nodes(0, 0, 4);
+    ASSERT_EQ(0, errorCode);
+
+    // Get the new state
+    meshkernelapi::MeshGeometryDimensions meshGeometryDimensions{};
+    meshkernelapi::MeshGeometry meshGeometry{};
+    errorCode = mkernel_get_mesh(0, meshGeometryDimensions, meshGeometry);
+
+    // Assert
+    ASSERT_EQ(0, errorCode);
+    ASSERT_EQ(8, meshGeometryDimensions.numnode);
+    ASSERT_EQ(10, meshGeometryDimensions.numedge);
+}
+
+TEST_F(ApiTest, MergeNodesThroughApi)
+{
+    // Set a new mesh in Mesh Kernel
+    MakeMesh();
+
+    // Execute
+    meshkernelapi::GeometryList geometry_list{};
+    auto errorCode = meshkernelapi::mkernel_merge_nodes(0, geometry_list);
+    ASSERT_EQ(0, errorCode);
+
+    // Get the new state
+    meshkernelapi::MeshGeometryDimensions meshGeometryDimensions{};
+    meshkernelapi::MeshGeometry meshGeometry{};
+    errorCode = mkernel_get_mesh(0, meshGeometryDimensions, meshGeometry);
+
+    // Assert (nothing is done, we just check that the api communication works)
+    ASSERT_EQ(0, errorCode);
+    ASSERT_EQ(9, meshGeometryDimensions.numnode);
+    ASSERT_EQ(12, meshGeometryDimensions.numedge);
+}
+
+TEST_F(ApiTest, OrthogonalizationThroughApi)
+{
+    // Set a new mesh in Mesh Kernel
+    MakeMesh();
+
+    // Execute
+    meshkernelapi::OrthogonalizationParameters orthogonalizationParameters{};
+    meshkernelapi::GeometryList geometryList{};
+    meshkernelapi::GeometryList landBoundaries{};
+
+    auto errorCode = mkernel_orthogonalize_initialize(0,
+                                                      1,
+                                                      orthogonalizationParameters,
+                                                      landBoundaries,
+                                                      geometryList);
+    ASSERT_EQ(0, errorCode);
+
+    errorCode = meshkernelapi::mkernel_orthogonalize_prepare_outer_iteration(0);
+    ASSERT_EQ(0, errorCode);
+
+    errorCode = meshkernelapi::mkernel_orthogonalize_inner_iteration(0);
+    ASSERT_EQ(0, errorCode);
+
+    errorCode = meshkernelapi::mkernel_orthogonalize_finalize_outer_iteration(0);
+    ASSERT_EQ(0, errorCode);
+
+    // Get the new state
+    meshkernelapi::MeshGeometryDimensions meshGeometryDimensions{};
+    meshkernelapi::MeshGeometry meshGeometry{};
+    errorCode = mkernel_get_mesh(0, meshGeometryDimensions, meshGeometry);
+
+    // Assert (nothing is done, we just check that the api communication works)
+    ASSERT_EQ(0, errorCode);
+    ASSERT_EQ(9, meshGeometryDimensions.numnode);
+    ASSERT_EQ(12, meshGeometryDimensions.numedge);
 }
