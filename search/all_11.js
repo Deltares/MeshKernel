27b34cfd6e5e625a46ce9c176d0ba2d3094a55b1
Hofer-Julian <Hofer-Julian@users.noreply.github.com>
2021-01-05 13:13:00 +0000

    deploy: 16da3dabe8dd7f4b4aeb3ed0e46c7a42b0e4e969

--- search/all_11.js
+++ search/all_11.js
@@ -5,7 +5,7 @@ var searchData=
   ['triangulatefaces',['TriangulateFaces',['../classmeshkernel_1_1_mesh.html#a1ab7e120468e45ef2d6e2eb8702cad90',1,'meshkernel::Mesh']]],
   ['triangulatepoints',['TriangulatePoints',['../structmeshkernel_1_1_triangulation_wrapper.html#a65184054217a86e1fb086622c75e1bafa3aac8b209f59ca7f9cb9f022bc72d76c',1,'meshkernel::TriangulationWrapper']]],
   ['triangulatepointsandgeneratefaces',['TriangulatePointsAndGenerateFaces',['../structmeshkernel_1_1_triangulation_wrapper.html#a65184054217a86e1fb086622c75e1bafa8fd89bb22640c4c53333ab29b9f84207',1,'meshkernel::TriangulationWrapper']]],
-  ['triangulation',['Triangulation',['../namespacemeshkernel.html#a5a4e91c351aad4f859d9ac69f026d29e',1,'meshkernel::Triangulation()'],['../namespacemeshkernelapi.html#a26197cf8eb6260749322541b7a027007',1,'meshkernelapi::triangulation()']]],
+  ['triangulation',['triangulation',['../namespacemeshkernelapi.html#a26197cf8eb6260749322541b7a027007',1,'meshkernelapi::triangulation()'],['../namespacemeshkernel.html#a5a4e91c351aad4f859d9ac69f026d29e',1,'meshkernel::Triangulation()']]],
   ['triangulationinterpolation',['TriangulationInterpolation',['../classmeshkernel_1_1_triangulation_interpolation.html',1,'meshkernel::TriangulationInterpolation'],['../classmeshkernel_1_1_triangulation_interpolation.html#a6b346ed9279976a221de5e4903fffc1c',1,'meshkernel::TriangulationInterpolation::TriangulationInterpolation()']]],
   ['triangulationoptions',['TriangulationOptions',['../structmeshkernel_1_1_triangulation_wrapper.html#a65184054217a86e1fb086622c75e1baf',1,'meshkernel::TriangulationWrapper']]],
   ['triangulationwrapper',['TriangulationWrapper',['../structmeshkernel_1_1_triangulation_wrapper.html',1,'meshkernel']]],
