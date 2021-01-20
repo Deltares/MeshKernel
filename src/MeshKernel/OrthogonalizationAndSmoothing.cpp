9c28d837d6dfbd79205f9b440b08978b4abd4a3f
Julian Hofer <julianhofer@gnome.org>
2021-01-20 11:49:46 +0100

    Fix merge conflict

--- src/MeshKernel/OrthogonalizationAndSmoothing.cpp
+++ src/MeshKernel/OrthogonalizationAndSmoothing.cpp
@@ -336,14 +336,11 @@ void meshkernel::OrthogonalizationAndSmoothing::ComputeCoordinates() const
 {
     //TODO :  implementation for m_mesh->m_projection == Projection::sphericalAccurate
     throw AlgorithmError("OrthogonalizationAndSmoothing::ComputeCoordinates: This functionality is not implemented yet.");
-<<<<<<< HEAD
 
     if (m_mesh->m_projection == Projection::sphericalAccurate)
     {
         throw AlgorithmError("OrthogonalizationAndSmoothing::ComputeCoordinates: This functionality is not implemented yet.");
     }
-=======
->>>>>>> master
 }
 
 void meshkernel::OrthogonalizationAndSmoothing::UpdateNodeCoordinates(size_t nodeIndex)
