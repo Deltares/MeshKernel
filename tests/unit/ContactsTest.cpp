d6249c618a071910543cc0de0b5f1db1357a0ee4
Julian Hofer <julianhofer@gnome.org>
2021-01-25 17:23:21 +0100

    First implementation of unit test
    Currently stack overflow in PointInWhichPolygon

--- tests/unit/ContactsTest.cpp
+++ tests/unit/ContactsTest.cpp
@@ -139,3 +139,132 @@ TEST(Contacts, ComputeMultipleConnections1dMeshInside2dMesh)
     ASSERT_EQ(7, contacts.m_mesh2dIndices[3]);
     ASSERT_EQ(8, contacts.m_mesh2dIndices[4]);
 }
+
+TEST(Contacts, ComputeConnectionsWithPolygons)
+{
+    // Create 1d mesh
+    std::vector<meshkernel::Point> nodes{
+        {144.578313, 230.636833},
+        {148.479997, 250.145254},
+        {152.326276, 269.66441},
+        {155.992653, 289.21842},
+        {163.108569, 307.723781},
+        {171.485584, 325.759464},
+        {180.71473, 343.361252},
+        {191.75036, 359.914696},
+        {203.615349, 375.861699},
+        {216.043509, 391.3969},
+        {232.539143, 402.399733},
+        {249.32359, 413.080745},
+        {266.401484, 423.278547},
+        {283.618936, 433.246546},
+        {302.83515, 437.07713},
+        {322.63432, 438.89845},
+        {342.529084, 438.89845},
+        {362.423848, 438.89845},
+        {382.318612, 438.89845},
+        {402.213377, 438.89845},
+        {422.108141, 438.89845},
+        {442.002905, 438.89845},
+        {461.654996, 441.830623},
+        {481.316269, 444.846117},
+        {501.057403, 447.313758},
+        {520.392165, 451.956905},
+        {539.189727, 458.275803},
+        {557.4759, 466.112735},
+        {574.862173, 475.610926},
+        {591.415617, 486.646556},
+        {607.969062, 497.682185},
+        {624.522506, 508.717815},
+        {641.145408, 519.647697},
+        {657.848458, 530.455553},
+        {670.513477, 545.357135},
+        {681.549107, 561.910579},
+        {692.85851, 578.277567},
+        {704.219703, 594.609283},
+        {713.16624, 612.094922},
+        {718.736774, 631.193896},
+        {724.244832, 650.310657},
+        {729.569544, 669.479619},
+        {734.250619, 688.809373},
+        {738.557122, 708.232391},
+        {742.655652, 727.700409},
+        {746.521516, 747.213434},
+        {749.978934, 766.805471},
+        {752.151462, 786.57487}};
+
+    std::vector<meshkernel::Edge> edges;
+    for (auto index = 0; index < nodes.size() - 1; ++index)
+    {
+        edges.emplace_back(meshkernel::Edge{index, index + 1});
+    }
+
+    const auto mesh1d = std::make_shared<meshkernel::Mesh1D>(edges, nodes, meshkernel::Projection::cartesian);
+
+    // Create 2d mesh
+    const auto mesh2d = MakeRectangularMeshForTesting(100, 100, 10, meshkernel::Projection::cartesian, {0.0, 0.0});
+
+    // Create contacts
+    std::vector<bool> onedNodeMask(nodes.size(), true);
+    meshkernel::Contacts contacts(mesh1d, mesh2d, onedNodeMask);
+
+    // Set the polygon where to generate the contacts
+    std::vector<meshkernel::Point> polygonPoints{
+        {260.004578, 180.002838},
+        {170.307068, 180.002838},
+        {169.094971, 272.124969},
+        {283.035034, 284.246307},
+        {300.004883, 205.457642},
+        {260.004578, 180.002838},
+        {meshkernel::doubleMissingValue, meshkernel::doubleMissingValue},
+        {212.731567, 422.429504},
+        {153.337280, 424.853821},
+        {149.700867, 520.612366},
+        {245.459045, 525.460876},
+        {268.489502, 474.551270},
+        {212.731567, 422.429504},
+        {meshkernel::doubleMissingValue, meshkernel::doubleMissingValue},
+        {483.036316, 307.276855},
+        {378.793213, 329.095245},
+        {404.247925, 403.035400},
+        {510.915283, 394.550476},
+        {483.036316, 307.276855},
+        {meshkernel::doubleMissingValue, meshkernel::doubleMissingValue},
+        {476.975647, 498.793945},
+        {434.551147, 495.157532},
+        {416.369202, 587.279663},
+        {526.672974, 589.703979},
+        {476.975647, 498.793945},
+        {meshkernel::doubleMissingValue, meshkernel::doubleMissingValue},
+        {719.401428, 370.307800},
+        {655.158630, 395.762604},
+        {652.734314, 475.763428},
+        {753.341003, 487.884766},
+        {719.401428, 370.307800},
+        {meshkernel::doubleMissingValue, meshkernel::doubleMissingValue},
+        {632.128113, 667.280518},
+        {575.158142, 732.735718},
+        {583.643005, 778.796753},
+        {645.461609, 792.130249},
+        {691.522522, 732.735718},
+        {632.128113, 667.280518},
+    };
+    meshkernel::Polygons polygon(polygonPoints, meshkernel::Projection::cartesian);
+
+    // Execute
+    contacts.ComputeConnectionsWithPolygons(polygon);
+
+    //Assert
+    ASSERT_EQ(6, contacts.m_mesh1dIndices.size());
+    ASSERT_EQ(6, contacts.m_mesh2dIndices.size());
+
+    // ASSERT_EQ(1, contacts.m_mesh1dIndices[0]);
+    // ASSERT_EQ(2, contacts.m_mesh1dIndices[1]);
+    // ASSERT_EQ(3, contacts.m_mesh1dIndices[2]);
+    // ASSERT_EQ(6, contacts.m_mesh1dIndices[3]);
+
+    // ASSERT_EQ(0, contacts.m_mesh2dIndices[0]);
+    // ASSERT_EQ(1, contacts.m_mesh2dIndices[1]);
+    // ASSERT_EQ(2, contacts.m_mesh2dIndices[2]);
+    // ASSERT_EQ(2, contacts.m_mesh2dIndices[3]);
+}
