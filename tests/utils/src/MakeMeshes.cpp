0c107144b2760ea3efb532c44b8c9bbc1047e0f1
lucacarniato <carniato.luca@gmail.com>
2021-02-02 17:06:16 +0100

    GRIDEDIT-290: use boost dll only in linux

--- tests/utils/src/MakeMeshes.cpp
+++ tests/utils/src/MakeMeshes.cpp
@@ -3,6 +3,8 @@
 #define NOMINMAX
 #endif
 #include <Windows.h>
+#else
+#include <boost/dll.hpp>
 #endif
 
 #include "../../../extern/netcdf/netCDF 4.6.1/include/netcdf.h"
@@ -14,6 +16,35 @@
 
 std::tuple<meshkernelapi::MeshGeometry, meshkernelapi::MeshGeometryDimensions> ReadLegacyMeshFromFileForApiTesting(std::string filePath)
 {
+
+#if _WIN32
+    auto netcdf = LoadLibrary("netcdf.dll");
+
+    if (!netcdf)
+    {
+        throw std::invalid_argument("ReadLegacyMeshFromFile: Could not load 'netcdf.dll'.");
+    }
+
+    typedef int(__stdcall * nc_open_dll)(const char* path, int mode, int* ncidp);
+    auto nc_open = (nc_open_dll)GetProcAddress(netcdf, "nc_open");
+
+    typedef int(__stdcall * nc_inq_dimid_dll)(int ncid, const char* name, int* idp);
+    auto nc_inq_dimid = (nc_inq_dimid_dll)GetProcAddress(netcdf, "nc_inq_dimid");
+
+    typedef int(__stdcall * nc_inq_dim_dll)(int ncid, int dimid, char* name, std::size_t* lenp);
+    auto nc_inq_dim = (nc_inq_dim_dll)GetProcAddress(netcdf, "nc_inq_dim");
+
+    typedef int(__stdcall * nc_inq_varid_dll)(int ncid, const char* name, int* varidp);
+    auto nc_inq_varid = (nc_inq_varid_dll)GetProcAddress(netcdf, "nc_inq_varid");
+
+    typedef int(__stdcall * nc_get_var_double_dll)(int ncid, int varid, double* ip);
+    auto nc_get_var_double = (nc_get_var_double_dll)GetProcAddress(netcdf, "nc_get_var_double");
+
+    typedef int(__stdcall * nc_get_var_int_dll)(int ncid, int varid, int* ip);
+    auto nc_get_var_int = (nc_get_var_int_dll)GetProcAddress(netcdf, "nc_get_var_int");
+
+#else
+
     boost::dll::shared_library lib("netcdf.dll");
 
     auto nc_open = lib.get<int(const char*, int, int*)>("nc_open");
@@ -22,6 +53,8 @@ std::tuple<meshkernelapi::MeshGeometry, meshkernelapi::MeshGeometryDimensions> R
     auto nc_inq_varid = lib.get<int(int, const char*, int*)>("nc_inq_varid");
     auto nc_get_var_double = lib.get<int(int, int, double*)>("nc_get_var_double");
     auto nc_get_var_int = lib.get<int(int, int, int*)>("nc_get_var_int");
+
+#endif
 
     int ncidp = 0;
     int err = nc_open(filePath.c_str(), NC_NOWRITE, &ncidp);
