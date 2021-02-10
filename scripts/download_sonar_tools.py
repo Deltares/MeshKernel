3b5511925942bcd6d249a57dd7dbb73571f927ad
Julian Hofer <julianhofer@gnome.org>
2021-02-10 10:26:00 +0100

    Update sonarcloud script

--- scripts/download_sonar_tools.py
+++ scripts/download_sonar_tools.py
@@ -1,7 +1,7 @@
-import requests 
+import requests
 import zipfile
 import shutil
-import argparse
+import platform
 
 from pathlib import Path
 
@@ -10,42 +10,46 @@ SONAR_SCANNER_VERSION = "4.4.0.2170"
 
 def download_file(url: str, save_path: Path, chunk_size=128) -> None:
     r = requests.get(url, stream=True)
-    with save_path.open('wb') as fd:
+    with save_path.open("wb") as fd:
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
 
 
 def unzip_file(zip_file_path: Path, unzip_directory: Path) -> None:
-    with zipfile.ZipFile(str(zip_file_path), 'r') as zip_ref:
+    with zipfile.ZipFile(str(zip_file_path), "r") as zip_ref:
         zip_ref.extractall(str(unzip_directory))
 
 
-def get_build_wrapper(save_dir: Path, os: str) -> None:
+def get_build_wrapper(save_dir: Path) -> None:
 
-    if os == "windows":
+    if platform.system() == "Windows":
         url = "https://sonarcloud.io/static/cpp/build-wrapper-win-x86.zip"
-        save_path = save_dir / Path("build-wrapper-win-x86.zip")
-    if os == "linux":
+    elif platform.system() == "Linux":
         url = "http://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip"
-        save_path = save_dir / Path("build-wrapper-win-x86.zip")
+    else:
+        raise Exception("Unsupported OS used.")
 
+    save_path = save_dir / Path("build-wrapper.zip")
     download_file(url, save_path)
     unzip_file(save_path, save_dir)
 
 
 def rename_sonar_scanner_folder(save_dir: Path) -> None:
     sonar_scanner_folder = next(save_dir.glob("sonar-scanner-*"))
-    shutil.move(str(sonar_scanner_folder), str(sonar_scanner_folder.with_name("sonar-scanner")))
+    shutil.move(
+        str(sonar_scanner_folder), str(sonar_scanner_folder.with_name("sonar-scanner"))
+    )
 
 
-def get_scanner(save_dir: Path, os: str) -> None:
-    if os == "windows":
+def get_scanner(save_dir: Path) -> None:
+    if platform.system() == "Windows":
         url = f"https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-{SONAR_SCANNER_VERSION}-windows.zip"
-        save_path = save_dir / Path("download_sonar_scanner.zip")
-    if os == "linux":
+    elif platform.system() == "Linux":
         url = f"https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-{SONAR_SCANNER_VERSION}-linux.zip"
-        save_path = save_dir / Path("download_sonar_scanner.zip")
+    else:
+        raise Exception("Unsupported OS used.")
 
+    save_path = save_dir / Path("download_sonar_scanner.zip")
     download_file(url, save_path)
     unzip_file(save_path, save_dir)
     rename_sonar_scanner_folder(save_dir)
@@ -54,15 +58,5 @@ def get_scanner(save_dir: Path, os: str) -> None:
 if __name__ == "__main__":
     save_dir = Path(".") / Path(".sonar")
     save_dir.mkdir(exist_ok=True)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--operating_system",
-                        choices=["windows","linux"],
-                        help="The operating system to use",
-                        default="windows",
-                        type=str)
-
-    args = parser.parse_args()
-
-    get_build_wrapper(save_dir, args.operating_system)
-    get_scanner(save_dir, args.operating_system)
+    get_build_wrapper(save_dir)
+    get_scanner(save_dir)
