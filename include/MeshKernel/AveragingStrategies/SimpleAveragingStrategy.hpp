fe3198c44099bf2c58be69930b2aa4b4ad2f2fe3
Maarten Tegelaers <BeardedPlatypus@users.noreply.github.com>
2021-03-17 14:22:19 +0100

    GRIDEDIT-338: Refactor averaging interpolation compute on polygon (#80)
    * Add new AveragingStrategy implementations
    
    * Add AveragingStrategy unit tests
    
    Co-authored-by: lucacarniato <carniato.luca@gmail.com>

---
+++ include/MeshKernel/AveragingStrategies/SimpleAveragingStrategy.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <MeshKernel/AveragingStrategies/AveragingStrategy.hpp>
+#include <MeshKernel/AveragingStrategies/SimpleAveragingStrategy.hpp>
+#include <MeshKernel/Entities.hpp>
+
+namespace meshkernel::averaging
+{
+    /// @brief SimpleAveragingStrategy implements the averaging strategy which takes the average of the values of all added points.
+    class SimpleAveragingStrategy final : public AveragingStrategy
+    {
+    public:
+        void Add(Point const& samplePoint, double sampleValue) override;
+        [[nodiscard]] double Calculate() const override;
+
+    private:
+        /// @brief The current result from which Calculate calculates the final value.
+        double m_result = 0.0;
+
+        /// @brief The number of times a value has been added to this strategy.
+        size_t m_nAdds = 0;
+    };
+} // namespace meshkernel::averaging
