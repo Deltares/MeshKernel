f9e6417c8b06a75aaca9e1572af1792df84dbadd
lucacarniato <carniato.luca@gmail.com>
2021-04-28 15:49:57 +0200

    GRIDEDIT-321: fix formatting

--- src/MeshKernel/Mesh1D.cpp
+++ src/MeshKernel/Mesh1D.cpp
@@ -32,6 +32,4 @@
 
 meshkernel::Mesh1D::Mesh1D(const std::vector<Edge>& edges,
                            const std::vector<Point>& nodes,
-                           Projection projection) : Mesh(edges, nodes, projection)
-{
-};
+                           Projection projection) : Mesh(edges, nodes, projection){};
