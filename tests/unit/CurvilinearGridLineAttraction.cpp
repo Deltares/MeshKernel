c241bcd90df7000cbd08265e270e8b779e698fc0
lucacarniato <carniato.luca@gmail.com>
2021-05-06 15:43:58 +0200

    GRIDEDIT-378: added unit test for attraction on n direction

--- tests/unit/CurvilinearGridLineAttraction.cpp
+++ tests/unit/CurvilinearGridLineAttraction.cpp
@@ -29,5 +29,31 @@ TEST(CurvilinearLineAttraction, Compute_OnMLine_ShouldAttractMLines)
     ASSERT_NEAR(366803.23746104678, modifiedGrid.m_gridNodes[2][2].y, tolerance);
     ASSERT_NEAR(366683.98469820933, modifiedGrid.m_gridNodes[3][2].y, tolerance);
     ASSERT_NEAR(366555.11052078847, modifiedGrid.m_gridNodes[4][2].y, tolerance);
+}
 
+TEST(CurvilinearLineAttraction, Compute_OnNLine_ShouldAttractNLines)
+{
+    // Set-up
+    const auto curvilinearGrid = MakeSmallCurvilinearGrid();
+    meshkernel::CurvilinearGridLineAttraction curvilinearLineAttraction(curvilinearGrid, 0.5);
+    curvilinearLineAttraction.SetLine({80198.2, 366750.6}, {80583.1, 366889.8});
+    curvilinearLineAttraction.SetBlock({80266.8, 367104.0}, {80419.3, 366566.2});
+
+    // Execute
+    const auto modifiedGrid = curvilinearLineAttraction.Compute();
+
+    // Asserts
+    const double tolerance = 1e-6;
+
+    ASSERT_NEAR(80145.970831448722, modifiedGrid.m_gridNodes[0][2].x, tolerance);
+    ASSERT_NEAR(80247.575117740766, modifiedGrid.m_gridNodes[1][2].x, tolerance);
+    ASSERT_NEAR(80292.449108019704, modifiedGrid.m_gridNodes[2][2].x, tolerance);
+    ASSERT_NEAR(80316.537053694148, modifiedGrid.m_gridNodes[3][2].x, tolerance);
+    ASSERT_NEAR(80331.200564142913, modifiedGrid.m_gridNodes[4][2].x, tolerance);
+
+    ASSERT_NEAR(367047.36276461056, modifiedGrid.m_gridNodes[0][2].y, tolerance);
+    ASSERT_NEAR(366897.17707224732, modifiedGrid.m_gridNodes[1][2].y, tolerance);
+    ASSERT_NEAR(366792.50812354451, modifiedGrid.m_gridNodes[2][2].y, tolerance);
+    ASSERT_NEAR(366703.03776077798, modifiedGrid.m_gridNodes[3][2].y, tolerance);
+    ASSERT_NEAR(366552.40947499714, modifiedGrid.m_gridNodes[4][2].y, tolerance);
 }
