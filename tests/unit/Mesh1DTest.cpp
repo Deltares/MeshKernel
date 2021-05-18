dfc9e90c791476c3692dceb66d7c88334f4bdc4a
lucacarniato <carniato.luca@gmail.com>
2021-05-18 14:37:09 +0200

    Add unit test GenerateMeshFromPolyLines_WithInexactOffset_ShouldGenerateMesh

--- tests/unit/Mesh1DTest.cpp
+++ tests/unit/Mesh1DTest.cpp
@@ -41,3 +41,42 @@ TEST(Mesh1D, GenerateMeshFromPolyLines_WithOverlappingNodes_ShouldRemoveOverlapp
     ASSERT_NEAR(5.0, mesh.m_nodes[7].y, tolerance);
     ASSERT_NEAR(10.0, mesh.m_nodes[8].y, tolerance);
 }
+
+TEST(Mesh1D, GenerateMeshFromPolyLines_WithInexactOffset_ShouldGenerateMesh)
+{
+    //1 Setup
+    std::vector<std::vector<meshkernel::Point>> polylines{
+        {{0.0, 0.0},
+         {10.0, 0.0},
+         {20.0, 0.0}},
+        {{10.0, -10.0},
+         {10.0, 0.0},
+         {10.0, 10.0}}};
+
+    // 2 Execution
+    const auto mesh = meshkernel::Mesh1D(polylines, 6.0, 0.01, meshkernel::Projection::cartesian);
+
+    // 3 Assertion
+    const auto tolerance = 1e-6;
+    ASSERT_NEAR(0.0, mesh.m_nodes[0].x, tolerance);
+    ASSERT_NEAR(6.0, mesh.m_nodes[1].x, tolerance);
+    ASSERT_NEAR(12.0, mesh.m_nodes[2].x, tolerance);
+    ASSERT_NEAR(18.0, mesh.m_nodes[3].x, tolerance);
+    ASSERT_NEAR(20.0, mesh.m_nodes[4].x, tolerance);
+    ASSERT_NEAR(10.0, mesh.m_nodes[5].x, tolerance);
+    ASSERT_NEAR(10.0, mesh.m_nodes[6].x, tolerance);
+    ASSERT_NEAR(10.0, mesh.m_nodes[7].x, tolerance);
+    ASSERT_NEAR(10.0, mesh.m_nodes[8].x, tolerance);
+    ASSERT_NEAR(10.0, mesh.m_nodes[9].x, tolerance);
+
+    ASSERT_NEAR(0.0, mesh.m_nodes[0].y, tolerance);
+    ASSERT_NEAR(0.0, mesh.m_nodes[1].y, tolerance);
+    ASSERT_NEAR(0.0, mesh.m_nodes[2].y, tolerance);
+    ASSERT_NEAR(0.0, mesh.m_nodes[3].y, tolerance);
+    ASSERT_NEAR(0.0, mesh.m_nodes[4].y, tolerance);
+    ASSERT_NEAR(-10.0, mesh.m_nodes[5].y, tolerance);
+    ASSERT_NEAR(-4.0, mesh.m_nodes[6].y, tolerance);
+    ASSERT_NEAR(2.0, mesh.m_nodes[7].y, tolerance);
+    ASSERT_NEAR(8.0, mesh.m_nodes[8].y, tolerance);
+    ASSERT_NEAR(10.0, mesh.m_nodes[9].y, tolerance);
+}
