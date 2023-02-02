da5aa8b245009af4c1310127db1c22fb2bf3611f
ahmad <ahmad.elsayed@deltares.nl>
2023-02-02 15:50:22 +0100

    Separated unit tests from performance tests

--- tests/benchmark/performance/src/main.cpp
+++ tests/benchmark/performance/src/main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 
 #include <benchmark/benchmark.h>
-#include <gtest/gtest.h>
 
-// #define DO_MANAGE_MEMORY true
 #include "benchmark_memory_manager.hpp"
-// #include "custom_memory_management.cpp"
 #include "custom_memory_management.hpp"
 
 int main(int argc, char** argv)
 {
-    // Unit tests
-    ::testing::InitGoogleTest(&argc, argv);
-    if (RUN_ALL_TESTS() != 0)
-    {
-        return EXIT_FAILURE;
-    }
 
-    // Benchmarks
     if (!argv)
     {
         argc = 1;
