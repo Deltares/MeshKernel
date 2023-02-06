34cde74603ff5b623711184cbd9dc9b3519c48c8
ahmad <ahmad.elsayed@deltares.nl>
2023-02-06 16:45:31 +0100

    Bumped CMake version to 3.23.1 in module load script

--- scripts/module_load.sh
+++ scripts/module_load.sh
@@ -1,7 +1,7 @@
 # Script that is meant to be sourced in the CI
 # It includes the necessary module loads to build MeshKernel on Deltares Linux machines
 
-module load cmake/3.18.0_gcc9.2.0
+module load cmake/3.23.1_gcc11.3.0
 module load gcc/9.2.0
 module load boost/1.73.0_gcc9.2.0
 module load netcdf/v4.7.4_v4.5.3_gcc9.2.0
