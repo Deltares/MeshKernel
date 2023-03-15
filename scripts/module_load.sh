98544e5dcd639d5099ac4c7b7b2fc41aa02639f8
ahmad <ahmad.elsayed@deltares.nl>
2023-03-15 19:10:27 +0100

    Cleaned up CMake config of target Triangle

--- scripts/module_load.sh
+++ scripts/module_load.sh
@@ -2,6 +2,6 @@
 # It includes the necessary module loads to build MeshKernel on Deltares Linux machines
 
 module load cmake/3.23.1_gcc11.3.0
-module load gcc/11.3.0 
+module load gcc/11.3.0
 module load boost/1.81.0_gcc11.3.0
 module load netcdf/v4.9.1_gcc11.3.0
