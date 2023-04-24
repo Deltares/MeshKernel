0dc8bad15e3eb221d436e7c06e3fe110716ed61a
ahmad <ahmad.elsayed@deltares.nl>
2023-04-24 15:00:46 +0200

    Improved var names and updated the doc

--- scripts/gather_to_sign_dll.py
+++ scripts/gather_to_sign_dll.py
@@ -18,7 +18,7 @@ import argparse
 import json
 
 
-def determine_to_sign_dll_names(repo_root: Path) -> Generator[str, None, None]:
+def determine_to_sign_dll_names(build_dir: Path) -> Generator[str, None, None]:
     """
     Gather all the dll names which need to be signed.
 
@@ -27,70 +27,70 @@ def determine_to_sign_dll_names(repo_root: Path) -> Generator[str, None, None]:
     a NuGet package, which should already be signed to begin with.
 
     Args:
-        repo_root (Path): Path to the repo root
+        build_dir (Path): Path to the build dir
 
     Returns:
         Generator[str, None, None]: Generator containing the dll names to sign.
     """
-    libs_vcxprojs = (repo_root / Path("libs")).glob("**/*.vcxproj")
+    libs_vcxprojs = (build_dir / Path("libs")).glob("**/*.vcxproj")
     return (x.with_suffix(".dll").name for x in libs_vcxprojs)
 
 
-def get_corresponding_path(repo_root: Path, dll_name: str) -> Path:
+def get_corresponding_path(build_dir: Path, dll_name: str) -> Path:
     """
-    Get the dll path corresponding with the dll_name from the provided repo_root.
+    Get the dll path corresponding with the dll_name from the provided build_dir.
 
     Args:
-        repo_root (Path): Path to the repo root
+        build_dir (Path): Path to the build dir
         dll_name (str): Name of the dll
     """
     # this hack is required to keep the capitalisation
     return next(
-        (repo_root / Path("libs")).glob("**/Release/{}".format(dll_name))
+        (build_dir / Path("libs")).glob("**/Release/{}".format(dll_name))
     ).parent / Path(dll_name)
 
 
-def has_corresponding_path(repo_root: Path, dll_name: str) -> bool:
+def has_corresponding_path(build_dir: Path, dll_name: str) -> bool:
     """
     Verify whether the provided dll has a corresponding path.
     Args:
-        repo_root (Path): Path to the repo root
+        build_dir (Path): Path to the build dir
         dll_name (Generator[str, None, None]): Generator containing the dll names to sign.
 
     Returns:
         bool: True if a corresponding path can be found; False otherwise.
     """
-    return any((repo_root / Path("libs")).glob(f"**/Release/{dll_name}"))
+    return any((build_dir / Path("libs")).glob(f"**/Release/{dll_name}"))
 
 
 def find_all_dll_paths(
-    repo_root: Path, dll_names: Generator[str, None, None]
+    build_dir: Path, dll_names: Generator[str, None, None]
 ) -> Generator[Path, None, None]:
     """
     Find the dll paths of the dll names provided in dll_names.
 
     Args:
-        repo_root (Path): Path to the repo root
+        build_dir (Path): Path to the build dir
         dll_names (Generator[str, None, None]): Generator containing the dll names to sign.
 
     Returns:
         Generator[Path, None, None]: Generator containing the dll paths to sign.
     """
     return (
-        get_corresponding_path(repo_root, x)
+        get_corresponding_path(build_dir, x)
         for x in dll_names
-        if has_corresponding_path(repo_root, x)
+        if has_corresponding_path(build_dir, x)
     )
 
 
 def generate_json_mapping_content(
-    repo_root: Path, dll_paths: Generator[Path, None, None]
+    build_dir: Path, dll_paths: Generator[Path, None, None]
 ) -> dict:
     """
     Generate the json mapping file, with which the signed dlls can be restored to their original paths.
 
     Args:
-        repo_root (Path): Path to the repo root
+        build_dir (Path): Path to the build dir
         dll_paths (Generator[Path, None, None]): Generator containing the dll names to sign.
 
     Returns:
@@ -98,7 +98,7 @@ def generate_json_mapping_content(
     """
     return {
         "mapping": list(
-            ({"dll": p.name, "path": str(p.relative_to(repo_root))} for p in dll_paths)
+            ({"dll": p.name, "path": str(p.relative_to(build_dir))} for p in dll_paths)
         )
     }
 
@@ -146,53 +146,53 @@ def harvest_to_sign_dlls(
         shutil.move(str(p), str(sign_dir))
 
 
-def restore_signed_dlls(repo_root: Path, dll_mapping: dict, sign_dir) -> None:
+def restore_signed_dlls(build_dir: Path, dll_mapping: dict, sign_dir) -> None:
     """
     Restore the signed dlls to their original locations.
     Args:
-        repo_root (Path): The path to the repo root.
+        build_dir (Path): The path to the build dir.
         dll_mapping (dict): Dictionary describing the mapping of dlls
         sign_dir (Path): Path to the directory containing the signed dlls.
     """
     for dll_info in dll_mapping["mapping"]:
         shutil.move(
-            str(sign_dir / dll_info["dll"]), str(repo_root / Path(dll_info["path"]))
+            str(sign_dir / dll_info["dll"]), str(build_dir / Path(dll_info["path"]))
         )
 
 
 TO_SIGN_DIR_NAME = "to_sign"
 
 
-def harvest(repo_root: Path) -> None:
+def harvest(build_dir: Path) -> None:
     """
     Create a to_sign folder containing all the dlls to sign, and their corresponding mapping
 
     Args:
-        repo_root: Path to the repo root.
+        build_dir: Path to the build dir.
     """
-    dll_names = determine_to_sign_dll_names(repo_root)
-    dll_paths = list(find_all_dll_paths(repo_root, dll_names))
+    dll_names = determine_to_sign_dll_names(build_dir)
+    dll_paths = list(find_all_dll_paths(build_dir, dll_names))
 
-    sign_dir = repo_root / Path(TO_SIGN_DIR_NAME)
+    sign_dir = build_dir / Path(TO_SIGN_DIR_NAME)
     if not (sign_dir.exists() and sign_dir.is_dir()):
         sign_dir.mkdir(parents=True)
 
-    dll_mapping = generate_json_mapping_content(repo_root, dll_paths)
+    dll_mapping = generate_json_mapping_content(build_dir, dll_paths)
     write_json_mapping_file(sign_dir, dll_mapping)
 
     harvest_to_sign_dlls(dll_paths, sign_dir)
 
 
-def restore(repo_root: Path) -> None:
+def restore(build_dir: Path) -> None:
     """
     Restore the signed dlls to their original locations.
     Args:
-        repo_root: Path to the repo root.
+        build_dir: Path to the build dir.
     """
-    sign_dir = repo_root / Path(TO_SIGN_DIR_NAME)
+    sign_dir = build_dir / Path(TO_SIGN_DIR_NAME)
     dll_mapping = read_json_mapping_file(sign_dir)
 
-    restore_signed_dlls(repo_root, dll_mapping, sign_dir)
+    restore_signed_dlls(build_dir, dll_mapping, sign_dir)
 
 
 def parse_arguments():
@@ -202,7 +202,7 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("repo_root_path", help="Path to the root of the repository.")
+    parser.add_argument("build_dir_path", help="Path to the root of the repository.")
     action_group = parser.add_mutually_exclusive_group(required=True)
     action_group.add_argument("--harvest", action="store_true")
     action_group.add_argument("--restore", action="store_false")
@@ -212,9 +212,9 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    repo_root = Path(args.repo_root_path)
+    build_dir = Path(args.build_dir_path)
 
     if args.harvest:
-        harvest(repo_root)
+        harvest(build_dir)
     else:
-        restore(repo_root)
+        restore(build_dir)
