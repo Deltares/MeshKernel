ca7a16ec1e99ffeb1c24cd340f525b43e570f194
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-07-07 09:13:35 +0200

    Fixed google test tag

--- cmake/fetch_content.cmake
+++ cmake/fetch_content.cmake
@@ -5,7 +5,7 @@ if(ENABLE_UNIT_TESTING)
   FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG release-1.13.0)
+  GIT_TAG v1.13.0)
 
   FetchContent_GetProperties(googletest)
   if(NOT googletest_POPULATED)
