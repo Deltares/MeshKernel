382d852bf2ef6799b6743bb5ee466514b0366895
BillSenior <89970704+BillSenior@users.noreply.github.com>
2023-07-14 12:42:14 +0200

    Feature/gridedit 510 polygon ref not working (#192)
    Fixed polygon refinement (#192 | GRIDEDIT-510)

--- libs/MeshKernel/include/MeshKernel/Exceptions.hpp
+++ libs/MeshKernel/include/MeshKernel/Exceptions.hpp
@@ -200,7 +200,7 @@ namespace meshkernel
     class AlgorithmError final : public MeshKernelError
     {
     public:
-        // @brief Class constructor parametrized by a variadic error message and optionally the source location.
+        /// @brief Class constructor parametrized by a variadic error message and optionally the source location.
         /// @param[in] message         The variadic error message.
         /// @param[in] source_location The source location.
         AlgorithmError(VariadicErrorMessage const& message,
@@ -222,6 +222,23 @@ namespace meshkernel
         /// @brief Returns the error category.
         /// @return The  error category.
         std::string Category() const override { return "AlgorithmError"; }
+    };
+
+    /// @brief An exception class thrown when an attempt is made that violates a range constraint.
+    ///
+    /// 1. When an index is out of bounds or violates a range constraint.
+    /// 2. Attempt to retrieve a component that does not exist.
+    /// 3. Attempt to dereference a null pointer.
+    class ConstraintError final : public MeshKernelError
+    {
+    public:
+        /// @brief ConstraintError constructor
+        using MeshKernelError::MeshKernelError;
+
+    private:
+        /// @brief Returns the error category.
+        /// @return The  error category.
+        std::string Category() const override { return "ConstraintError"; }
     };
 
     /// @brief A class for throwing mesh geometry errors.
