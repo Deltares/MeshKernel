4f5b34c2572083e321b8d2fee9784223a4f21374
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-08-21 17:34:35 +0200

    Added libeigen ops resize, erase and insert with unit tests

--- libs/MeshKernel/include/MeshKernel/Utilities/LinearAlgebra.hpp
+++ libs/MeshKernel/include/MeshKernel/Utilities/LinearAlgebra.hpp
@@ -2,6 +2,9 @@
 
 #include <Eigen/Core>
 
+#include <stdexcept>
+#include <vector>
+
 namespace lin_alg
 {
     /// @brief Row major dynamic matrix
@@ -18,5 +21,248 @@ namespace lin_alg
     /// @tparam T Data type
     template <class T>
     using ColumnVector = Eigen::Matrix<T, Eigen::Dynamic, 1>;
+
+    /// @brief  Dynamic vector
+    /// @tparam T Data type
+    template <typename T>
+    using Vector = Eigen::Matrix<T, 1, Eigen::Dynamic, Eigen::RowMajor>;
+
+    /// @brief     Checks if a matrix is empty by inspecting its size
+    /// @tparam    T      The type of the matrix elements
+    /// @param[in] matrix The matrix
+    /// @return    True if the matrix is empty, false otherwise
+    template <typename T>
+    [[nodiscard]] inline static bool MatrixIsEmpty(MatrixRowMajor<T> const& matrix)
+    {
+        return matrix.size() == 0;
+    }
+
+    /// @brief         Resizes and fills a matrix
+    /// @tparam        T        The data type of the matrix elements
+    /// @param[in,out] matrix   The matrix
+    /// @param[in]     rows     The number of rows
+    /// @param[in]     cols     The number of columns
+    /// @param[in]     preserve Optional parameter that determines if the existing elements in
+    ///                         the matrix should be preserved or overwritten by the provided
+    ///                         or default (if not provided) fill value
+    /// @param[in]     fill_value The optional fill value (defaults to the template class constructor)
+    /// @return        True if the input matrix has been resized, false otherwise
+    template <typename T>
+    inline static bool ResizeAndFillMatrix(MatrixRowMajor<T>& matrix,
+                                           Eigen::Index rows,
+                                           Eigen::Index cols,
+                                           bool preserve = false,
+                                           T const& fill_value = {})
+    {
+        if (rows < 0 || cols < 0)
+        {
+            throw std::invalid_argument("Invalid range");
+        }
+
+        Eigen::Index const rows_old = matrix.rows();
+        Eigen::Index const cols_old = matrix.cols();
+        bool const resize = (rows != rows_old) || (cols != cols_old);
+        if (!resize)
+        {
+            if (!preserve)
+            {
+                matrix.fill(fill_value);
+            }
+        }
+        else
+        {
+            if (!preserve)
+            {
+                matrix.resize(rows, cols);
+                matrix.fill(fill_value);
+            }
+            else
+            {
+                matrix.conservativeResize(rows, cols);
+                if (rows > rows_old)
+                {
+                    matrix.bottomRows(rows - rows_old).setConstant(fill_value);
+                }
+                if (cols > cols_old)
+                {
+                    matrix.topRightCorner(rows_old, cols - cols_old).setConstant(fill_value);
+                }
+            }
+        }
+        return resize;
+    }
+
+    /// @brief         Erases a range of rows from a matrix given the begin and end row indices
+    /// @tparam        T         The type of the matrix elements
+    /// @param[in,out] matrix    The matrix
+    /// @param[in]     col_begin The begin row index
+    /// @param[in]     col_end   The end row index
+    template <typename T>
+    inline static void EraseRows(MatrixRowMajor<T>& matrix,
+                                 Eigen::Index row_begin,
+                                 Eigen::Index row_end)
+    {
+        if (row_begin < 0 ||
+            row_end < row_begin ||
+            row_end > matrix.rows() - 1)
+        {
+            throw std::invalid_argument("Invalid range");
+        }
+
+        Eigen::Index const rows_to_remove = row_end - row_begin + 1;
+        Eigen::Index const rows_new = matrix.rows() - rows_to_remove;
+        Eigen::Index const rows_to_move_up = rows_new - row_begin;
+        Eigen::Index const cols = matrix.cols();
+
+        // move the block of rows to keep to the position of the block of rows to remove
+        matrix.block(row_begin, 0, rows_to_move_up, cols) =
+            matrix.block(row_end + 1, 0, rows_to_move_up, cols);
+
+        // resize and preserve matrix contents
+        matrix.conservativeResize(rows_new, cols);
+    }
+
+    /// @brief        Erases a row from a matrix given the row index
+    /// @tparam        T      The type of the matrix elements
+    /// @param[in,out] matrix The matrix
+    /// @param[in]     row    The row index
+    template <typename T>
+    inline static void EraseRow(MatrixRowMajor<T>& matrix, Eigen::Index row)
+    {
+        EraseRows(matrix, row, row);
+    }
+
+    /// @brief         Erases a range of columns from a matrix given the begin and end column indices
+    /// @tparam        T         The type of the matrix elements
+    /// @param[in,out] matrix    The matrix
+    /// @param[in]     col_begin The begin column index
+    /// @param[in]     col_end   The end column index
+    template <typename T>
+    inline static void EraseCols(MatrixRowMajor<T>& matrix,
+                                 Eigen::Index col_begin,
+                                 Eigen::Index col_end)
+    {
+        if (col_begin < 0 ||
+            col_end < col_begin ||
+            col_end > matrix.cols() - 1)
+        {
+            throw std::invalid_argument("Invalid range");
+        }
+
+        Eigen::Index const rows = matrix.rows();
+        Eigen::Index const cols_to_remove = col_end - col_begin + 1;
+        Eigen::Index const cols_new = matrix.cols() - cols_to_remove;
+        Eigen::Index const cols_to_move_left = cols_new - col_begin;
+
+        // move the block of columns to keep to the position of the block of columns to remove
+        matrix.block(0, col_begin, rows, cols_to_move_left) =
+            matrix.block(0, col_end + 1, rows, cols_to_move_left);
+
+        // resize and preserve matrix contents
+        matrix.conservativeResize(rows, cols_new);
+    }
+
+    /// @brief         Erases a column from a matrix given the column index
+    /// @tparam        T      The type of the matrix elements
+    /// @param[in,out] matrix The matrix
+    /// @param[in]     col    The column index
+    template <typename T>
+    inline static void EraseCol(MatrixRowMajor<T>& matrix, Eigen::Index col)
+    {
+        EraseCols(matrix, col, col);
+    }
+
+    /// @brief         Inserts an Eigen vector in a matrix at a given row index
+    /// @tparam        T      The type of the matrix elements
+    /// @param[in,out] matrix The matrix
+    /// @param[in]     vector The vector to insert
+    /// @param[in]     row    The row index where the vector is to be inserted in the matrix
+    template <typename T>
+    inline static void InsertRow(MatrixRowMajor<T>& matrix,
+                                 Vector<T> const& vector,
+                                 Eigen::Index row)
+    {
+        if (row < 0 || row > matrix.rows())
+        {
+            throw std::invalid_argument("Invalid range");
+        }
+
+        Eigen::Index const rows_new = matrix.rows() + 1;
+        Eigen::Index const rows_to_move_down = matrix.rows() - row;
+        Eigen::Index const cols = matrix.cols();
+
+        auto const block_to_move_down = matrix.block(row, 0, rows_to_move_down, cols).eval();
+        matrix.conservativeResize(rows_new, cols);
+        matrix.block(row, 0, 1, cols) = vector;
+        matrix.block(row + 1, 0, rows_to_move_down, cols) = block_to_move_down;
+    }
+
+    /// @brief     Transforms a STL vector to an Eigen row-major vector
+    /// @tparam    T      The type of the matrix elements
+    /// @param[in] vector The STL vector
+    /// @return    The Eigen row-major vector
+    template <typename T>
+    inline static Vector<T> STLVectorToVector(std::vector<T> const& stl_vector)
+    {
+        // Vector<T> vector(1, stl_vector.size());
+        // for (Eigen::Index i = 0; i < vector.cols(); ++i)
+        // {
+        //     vector(0, i) = stl_vector[i];
+        // }
+        // return vector;
+
+        return Eigen::Map<Vector<T> const>(stl_vector.data(), 1, stl_vector.size());
+    }
+
+    /// @brief         Inserts a STL vector in a matrix at a given row index
+    /// @tparam        T      The type of the matrix elements
+    /// @param[in,out] matrix     The matrix
+    /// @param[in]     stl_vector The STL vector to insert
+    /// @param[in]     row    The row index where the vector is to be inserted in the matrix
+    template <typename T>
+    inline static void InsertRow(MatrixRowMajor<T>& matrix,
+                                 std::vector<T> const& stl_vector,
+                                 Eigen::Index row)
+    {
+        InsertRow(matrix, STLVectorToVector(stl_vector), row);
+    }
+
+    /// @brief         Inserts an Eigen vector in a matrix at a given column index
+    /// @tparam        T      The type of the matrix elements
+    /// @param[in,out] matrix The matrix
+    /// @param[in]     vector The vector to insert
+    /// @param[in]     col    The column index where the vector is to be inserted in the matrix
+    template <typename T>
+    inline static void InsertCol(MatrixRowMajor<T>& matrix,
+                                 Vector<T> const& vector,
+                                 Eigen::Index col)
+    {
+        if (col < 0 || col > matrix.cols())
+        {
+            throw std::invalid_argument("Invalid range");
+        }
+
+        Eigen::Index const rows = matrix.rows();
+        Eigen::Index const cols_new = matrix.cols() + 1;
+        Eigen::Index const cols_to_move_right = matrix.cols() - col;
+
+        auto const block_to_move_right = matrix.block(0, col, rows, cols_to_move_right).eval();
+        matrix.conservativeResize(rows, cols_new);
+        matrix.block(0, col, rows, 1) = vector.transpose();
+        matrix.block(0, col + 1, rows, cols_to_move_right) = block_to_move_right;
+    }
+
+    /// @brief         Inserts a STL vector in a matrix at a given column index
+    /// @tparam        T      The type of the matrix elements
+    /// @param[in,out] matrix     The matrix
+    /// @param[in]     stl_vector The STL vector to insert
+    /// @param[in]     col    The column index where the vector is to be inserted in the matrix
+    template <typename T>
+    inline static void InsertCol(MatrixRowMajor<T>& matrix,
+                                 std::vector<T> const& stl_vector,
+                                 Eigen::Index col)
+    {
+        InsertCol(matrix, STLVectorToVector(stl_vector), col);
+    }
 
 } // namespace lin_alg
---
+++ libs/MeshKernel/tests/src/LinearAlgebraTests.cpp
@@ -0,0 +1,532 @@
+#include <gtest/gtest.h>
+
+#include <MeshKernel/Utilities/LinearAlgebra.hpp>
+
+TEST(LinearAlgebra, Empty)
+{
+    {
+        lin_alg::MatrixRowMajor<int> matrix;
+        EXPECT_TRUE(lin_alg::MatrixIsEmpty(matrix));
+    }
+
+    {
+        lin_alg::MatrixRowMajor<int> matrix(3, 4);
+        EXPECT_FALSE(lin_alg::MatrixIsEmpty(matrix));
+    }
+}
+
+TEST(LinearAlgebra, Resize)
+{
+    lin_alg::MatrixRowMajor<int> matrix;
+
+    EXPECT_THROW(lin_alg::ResizeAndFillMatrix(matrix, -1, -1), std::invalid_argument);
+
+    {
+        Eigen::Index constexpr rows = 3;
+        Eigen::Index constexpr cols = 4;
+
+        EXPECT_TRUE(lin_alg::ResizeAndFillMatrix(matrix, rows, cols)); // 3x4
+        EXPECT_EQ(matrix.rows(), rows);
+        EXPECT_EQ(matrix.cols(), cols);
+
+        // fill value was not set, expect default int initialisation
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix.setZero();
+        EXPECT_EQ(matrix, expected_matrix);
+
+        // resize with the same dimensions
+        EXPECT_FALSE(lin_alg::ResizeAndFillMatrix(matrix, rows, cols));
+    }
+
+    // Add extra row and extra col and fill with ones without preserving
+    {
+        Eigen::Index rows_new = matrix.rows() + 1;
+        Eigen::Index cols_new = matrix.cols() + 1;
+        EXPECT_TRUE(lin_alg::ResizeAndFillMatrix(matrix, rows_new, cols_new, false, 1)); // 4x5
+        EXPECT_EQ(matrix.rows(), rows_new);
+        EXPECT_EQ(matrix.cols(), cols_new);
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows_new, cols_new); // 4x5
+        expected_matrix.setOnes();
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // Add extra row and extra col and fill with 2s with preserving
+    {
+        Eigen::Index rows_new = matrix.rows() + 1;
+        Eigen::Index cols_new = matrix.cols() + 1;
+        EXPECT_TRUE(lin_alg::ResizeAndFillMatrix(matrix, rows_new, cols_new, true, 2)); // 5x6
+        EXPECT_EQ(matrix.rows(), rows_new);
+        EXPECT_EQ(matrix.cols(), cols_new);
+
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows_new, cols_new); // 5x6
+        expected_matrix.setOnes();
+        expected_matrix.bottomRows(1).setConstant(2);
+        expected_matrix.rightCols(1).setConstant(2);
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // shrink by one row and one column with preserving
+    {
+        Eigen::Index rows_new = matrix.rows() - 1;
+        Eigen::Index cols_new = matrix.cols() - 1;
+        EXPECT_TRUE(lin_alg::ResizeAndFillMatrix(matrix, rows_new, cols_new, true)); // 4x5
+        EXPECT_EQ(matrix.rows(), rows_new);
+        EXPECT_EQ(matrix.cols(), cols_new);
+        {
+            lin_alg::MatrixRowMajor<int> expected_matrix(rows_new, cols_new); // 4x5
+            expected_matrix.setOnes();
+            EXPECT_EQ(matrix, expected_matrix);
+        }
+    }
+
+    // add 3 extra cols, preserve old matrix and fill 2ith 666
+    {
+        Eigen::Index rows_new = matrix.rows();
+        Eigen::Index cols_new = matrix.cols() + 3;
+        EXPECT_TRUE(lin_alg::ResizeAndFillMatrix(matrix, rows_new, cols_new, true, 666)); // 4x8
+        EXPECT_EQ(matrix.rows(), rows_new);
+        EXPECT_EQ(matrix.cols(), cols_new);
+        {
+            lin_alg::MatrixRowMajor<int> expected_matrix(rows_new, cols_new); // 4x8
+            expected_matrix.setOnes();
+            expected_matrix.rightCols(3).setConstant(666);
+            EXPECT_EQ(matrix, expected_matrix);
+        }
+    }
+
+    // add 4 extra rows, preserve old matrix and fill with -666
+    {
+        Eigen::Index rows_new = matrix.rows() + 4;
+        Eigen::Index cols_new = matrix.cols();
+        EXPECT_TRUE(lin_alg::ResizeAndFillMatrix(matrix, rows_new, cols_new, true, -666)); // 8x8
+        EXPECT_EQ(matrix.rows(), rows_new);
+        EXPECT_EQ(matrix.cols(), cols_new);
+        {
+            lin_alg::MatrixRowMajor<int> expected_matrix(rows_new, cols_new); // 8x5
+            expected_matrix.setOnes();
+            expected_matrix.rightCols(3).setConstant(666);
+            expected_matrix.bottomRows(4).setConstant(-666);
+            EXPECT_EQ(matrix, expected_matrix);
+        }
+    }
+}
+
+TEST(LinearAlgebra, EraseOneRow)
+{
+    lin_alg::MatrixRowMajor<int> matrix(5, 3);
+    matrix << 1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+        10, 11, 12,
+        13, 14, 15;
+
+    lin_alg::EraseRow(matrix, 2);
+    EXPECT_EQ(matrix.rows(), 4);
+    {
+        lin_alg::MatrixRowMajor<int> expected_matrix(4, 3);
+        expected_matrix << 1, 2, 3,
+            4, 5, 6,
+            10, 11, 12,
+            13, 14, 15;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    lin_alg::EraseRow(matrix, 1);
+    EXPECT_EQ(matrix.rows(), 3);
+    {
+        lin_alg::MatrixRowMajor<int> expected_matrix(3, 3);
+        expected_matrix << 1, 2, 3,
+            10, 11, 12,
+            13, 14, 15;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    lin_alg::EraseRow(matrix, 0);
+    EXPECT_EQ(matrix.rows(), 2);
+    {
+        lin_alg::MatrixRowMajor<int> expected_matrix(2, 3);
+        expected_matrix << 10, 11, 12,
+            13, 14, 15;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+}
+
+TEST(LinearAlgebra, EraseMultipleRows)
+{
+    Eigen::Index constexpr cols = 3;
+    lin_alg::MatrixRowMajor<int> matrix(6, cols);
+    matrix << 1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+        10, 11, 12,
+        13, 14, 15,
+        16, 17, 18;
+
+    lin_alg::EraseRows(matrix, 2, 3); // remove rows 2 and 3
+    EXPECT_EQ(matrix.rows(), 4);      // 6 -2 = 4 rows left
+    EXPECT_EQ(matrix.cols(), cols);   // unchanged
+    lin_alg::MatrixRowMajor<int> expected_matrix(4, cols);
+    expected_matrix << 1, 2, 3,
+        4, 5, 6,
+        13, 14, 15,
+        16, 17, 18;
+    EXPECT_EQ(matrix, expected_matrix);
+}
+
+TEST(LinearAlgebra, EraseOneCol)
+{
+    Eigen::Index constexpr rows = 3;
+    lin_alg::MatrixRowMajor<int> matrix(rows, 5);
+    matrix << 1, 2, 3, 4, 5,
+        6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15;
+
+    lin_alg::EraseCol(matrix, 2);
+    EXPECT_EQ(matrix.cols(), 4);
+    {
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, 4);
+        expected_matrix << 1, 2, 4, 5,
+            6, 7, 9, 10,
+            11, 12, 14, 15;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    lin_alg::EraseCol(matrix, 1);
+    EXPECT_EQ(matrix.cols(), 3);
+    {
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, 3);
+        expected_matrix << 1, 4, 5,
+            6, 9, 10,
+            11, 14, 15;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    lin_alg::EraseCol(matrix, 0);
+    EXPECT_EQ(matrix.cols(), 2);
+    {
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, 2);
+        expected_matrix << 4, 5,
+            9, 10,
+            14, 15;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+}
+
+TEST(LinearAlgebra, EraseMultipleCols)
+{
+    Eigen::Index constexpr rows = 3;
+    lin_alg::MatrixRowMajor<int> matrix(rows, 6);
+    matrix << 1, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12,
+        13, 14, 15, 16, 17, 18;
+    lin_alg::EraseCols(matrix, 2, 3); // remove cols 2 and 3
+    EXPECT_EQ(matrix.rows(), rows);   // unchanged
+    EXPECT_EQ(matrix.cols(), 4);      // 6 - 2 = 4 cols left
+    lin_alg::MatrixRowMajor<int> expected_matrix(rows, 4);
+    expected_matrix << 1, 2, 5, 6,
+        7, 8, 11, 12,
+        13, 14, 17, 18;
+    EXPECT_EQ(matrix, expected_matrix);
+}
+
+TEST(LinearAlgebra, InsertRowFromEigenVector)
+{
+    Eigen::Index rows = 1;
+    Eigen::Index constexpr cols = 3;
+    lin_alg::MatrixRowMajor<int> matrix(rows, cols);
+    matrix << 7, 8, 9;
+
+    // insert zeroth row
+    {
+        lin_alg::Vector<int> vector(cols);
+        vector << 1, 2, 3;
+        // row
+        // 0 : 1, 2, 3, <- insert at 0
+        // 1 : 7, 8, 9
+        lin_alg::InsertRow(matrix, vector, 0);
+        rows++;
+        EXPECT_EQ(matrix.rows(), rows); // 1+1 rows
+        EXPECT_EQ(matrix.cols(), cols); // unchanged
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 2, 3,
+            7, 8, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert at first row
+    {
+        lin_alg::Vector<int> vector(cols);
+        vector << 4, 5, 6;
+        // row
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6, <- insert at 1
+        // 2 : 7, 8, 9
+        lin_alg::InsertRow(matrix, vector, 1);
+        rows++;
+        EXPECT_EQ(matrix.rows(), rows); // 2+1 rows
+        EXPECT_EQ(matrix.cols(), cols); // unchanged
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 2, 3,
+            4, 5, 6,
+            7, 8, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert at third row
+    {
+        lin_alg::Vector<int> vector(cols);
+        vector << 10, 11, 12;
+        // row
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6,
+        // 2 : 7, 8, 9,
+        // 3 : 10, 11, 12  <- insert at 3
+        lin_alg::InsertRow(matrix, vector, 3);
+        rows++;
+        EXPECT_EQ(matrix.rows(), rows); // 3+1 rows
+        EXPECT_EQ(matrix.cols(), cols); // unchanged
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 2, 3,
+            4, 5, 6,
+            7, 8, 9,
+            10, 11, 12;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // throw out of range exceptions
+    {
+        lin_alg::Vector<int> vector(cols);
+        vector << 666, 666, 666;
+
+        // at the top side of the matrix
+        // row
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6,
+        // 2 : 7, 8, 9,
+        // 3 : 10, 11, 12
+        // 4 : can insert here but won't
+        // 5 : cannot insert here, should throw
+        EXPECT_THROW(lin_alg::InsertRow(matrix, vector, 5), std::invalid_argument);
+
+        // at the bottom side of the matrix
+        // row
+        // -1: cannot insert here, should throw
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6,
+        // 2 : 7, 8, 9,
+        // 3 : 10, 11, 12
+        EXPECT_THROW(lin_alg::InsertRow(matrix, vector, -1), std::invalid_argument);
+    }
+}
+
+TEST(LinearAlgebra, InsertRowFromSTLVector)
+{
+    Eigen::Index rows = 1;
+    Eigen::Index constexpr cols = 3;
+    lin_alg::MatrixRowMajor<int> matrix(rows, cols);
+    matrix << 7, 8, 9;
+
+    // insert zeroth row
+    {
+        std::vector<int> vector{1, 2, 3};
+        // row
+        // 0 : 1, 2, 3, <- insert at 0
+        // 1 : 7, 8, 9
+        lin_alg::InsertRow(matrix, vector, 0);
+        rows++;
+        EXPECT_EQ(matrix.rows(), rows); // 1+1 rows
+        EXPECT_EQ(matrix.cols(), cols); // unchanged
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 2, 3,
+            7, 8, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert at first row
+    {
+        std::vector<int> vector{4, 5, 6};
+        // row
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6, <- insert at 1
+        // 2 : 7, 8, 9
+        lin_alg::InsertRow(matrix, vector, 1);
+        rows++;
+        EXPECT_EQ(matrix.rows(), rows); // 2+1 rows
+        EXPECT_EQ(matrix.cols(), cols); // unchanged
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 2, 3,
+            4, 5, 6,
+            7, 8, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert at third row
+    {
+        std::vector<int> vector{10, 11, 12};
+        // row
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6,
+        // 2 : 7, 8, 9,
+        // 3 : 10, 11, 12  <- insert at 3
+        lin_alg::InsertRow(matrix, vector, 3);
+        rows++;
+        EXPECT_EQ(matrix.rows(), rows); // 3+1 rows
+        EXPECT_EQ(matrix.cols(), cols); // unchanged
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 2, 3,
+            4, 5, 6,
+            7, 8, 9,
+            10, 11, 12;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // throw out of range exceptions
+    {
+        std::vector<int> vector{666, 666, 666};
+
+        // at the bottom side of the matrix
+        // row
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6,
+        // 2 : 7, 8, 9,
+        // 3 : 10, 11, 12
+        // 4 : can insert here but won't
+        // 5 : cannot insert here, should throw
+        EXPECT_THROW(lin_alg::InsertRow(matrix, vector, 5), std::invalid_argument);
+
+        // at the top side of the matrix
+        // row
+        // -1: cannot insert here, should throw
+        // 0 : 1, 2, 3,
+        // 1 : 4, 5, 6,
+        // 2 : 7, 8, 9,
+        // 3 : 10, 11, 12
+        EXPECT_THROW(lin_alg::InsertRow(matrix, vector, -1), std::invalid_argument);
+    }
+}
+
+TEST(LinearAlgebra, InsertColFromEigenVector)
+{
+    Eigen::Index constexpr rows = 3;
+    Eigen::Index cols = 1;
+    lin_alg::MatrixRowMajor<int> matrix(rows, 1);
+    matrix << 7,
+        8,
+        9;
+
+    // insert zeroth column
+    {
+        lin_alg::Vector<int> vector(rows);
+        vector << 1, 2, 3;
+        lin_alg::InsertCol(matrix, vector, 0);
+        cols++;
+        EXPECT_EQ(matrix.rows(), rows); // unchanged
+        EXPECT_EQ(matrix.cols(), cols); // 1+1 cols
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 7,
+            2, 8,
+            3, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert first column
+    {
+        lin_alg::Vector<int> vector(rows);
+        vector << 4, 5, 6;
+        lin_alg::InsertCol(matrix, vector, 1);
+        cols++;
+        EXPECT_EQ(matrix.rows(), rows); // unchanged
+        EXPECT_EQ(matrix.cols(), cols); // 1+2 cols
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 4, 7,
+            2, 5, 8,
+            3, 6, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert third column
+    {
+        lin_alg::Vector<int> vector(rows);
+        vector << 10, 11, 12;
+        lin_alg::InsertCol(matrix, vector, 3);
+        cols++;
+        EXPECT_EQ(matrix.rows(), rows); // unchanged
+        EXPECT_EQ(matrix.cols(), cols); // 1+3 cols
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 4, 7, 10,
+            2, 5, 8, 11,
+            3, 6, 9, 12;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // throw out of range exceptions
+    {
+        lin_alg::Vector<int> vector(rows);
+        vector << 666, 666, 666;
+        // at the right side of the matrix
+        EXPECT_THROW(lin_alg::InsertCol(matrix, vector, 5), std::invalid_argument);
+        // at the left side of the matrix
+        EXPECT_THROW(lin_alg::InsertCol(matrix, vector, -1), std::invalid_argument);
+    }
+}
+
+TEST(LinearAlgebra, InsertColFromSTLVector)
+{
+    Eigen::Index constexpr rows = 3;
+    Eigen::Index cols = 1;
+    lin_alg::MatrixRowMajor<int> matrix(rows, 1);
+    matrix << 7,
+        8,
+        9;
+
+    // insert zeroth column
+    {
+        std::vector<int> vector{1, 2, 3};
+        lin_alg::InsertCol(matrix, vector, 0);
+        cols++;
+        EXPECT_EQ(matrix.rows(), rows); // unchanged
+        EXPECT_EQ(matrix.cols(), cols); // 1+1 cols
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 7,
+            2, 8,
+            3, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert first column
+    {
+        std::vector<int> vector{4, 5, 6};
+        lin_alg::InsertCol(matrix, vector, 1);
+        cols++;
+        EXPECT_EQ(matrix.rows(), rows); // unchanged
+        EXPECT_EQ(matrix.cols(), cols); // 1+2 cols
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 4, 7,
+            2, 5, 8,
+            3, 6, 9;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // insert third column
+    {
+        std::vector<int> vector{10, 11, 12};
+        lin_alg::InsertCol(matrix, vector, 3);
+        cols++;
+        EXPECT_EQ(matrix.rows(), rows); // unchanged
+        EXPECT_EQ(matrix.cols(), cols); // 1+3 cols
+        lin_alg::MatrixRowMajor<int> expected_matrix(rows, cols);
+        expected_matrix << 1, 4, 7, 10,
+            2, 5, 8, 11,
+            3, 6, 9, 12;
+        EXPECT_EQ(matrix, expected_matrix);
+    }
+
+    // throw out of range exceptions
+    {
+        std::vector<int> vector{666, 666, 666};
+        // at the right side of the matrix
+        EXPECT_THROW(lin_alg::InsertCol(matrix, vector, 5), std::invalid_argument);
+        // at the left side of the matrix
+        EXPECT_THROW(lin_alg::InsertCol(matrix, vector, -1), std::invalid_argument);
+    }
+}
