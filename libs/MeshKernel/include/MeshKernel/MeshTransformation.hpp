82b470f9b1652a8c4a6106a0270a7767f4b1c347
BillSenior <bill.senior@deltares.nl>
2023-10-23 18:32:30 +0200

    GRIDEDIT-758 Added mesh transformation.

---
+++ libs/MeshKernel/include/MeshKernel/MeshTransformation.hpp
@@ -0,0 +1,253 @@
+//---- GPL ---------------------------------------------------------------------
+//
+// Copyright (C)  Stichting Deltares, 2011-2023.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// contact: delft3d.support@deltares.nl
+// Stichting Deltares
+// P.O. Box 177
+// 2600 MH Delft, The Netherlands
+//
+// All indications and logos of, and references to, "Delft3D" and "Deltares"
+// are registered trademarks of Stichting Deltares, and remain the property of
+// Stichting Deltares. All rights reserved.
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include <type_traits>
+
+#include <Eigen/Core>
+
+#include "MeshKernel/Mesh.hpp"
+
+namespace meshkernel
+{
+
+    /// @brief Ensure any instantiation of the MeshTransformation Compute function is with the correct operation
+    template <typename Operation>
+    concept TransformationOperation = requires(Operation op, Point p) {{ op.apply(p)} -> std::same_as<Point>; };
+
+    /// @brief Apply a translation transformation to a point or a vector.
+    class Translation
+    {
+    public:
+        /// @brief Default constructor, default is no translation
+        Translation() = default;
+
+        /// @brief Construct with user defined translation
+        explicit Translation(const Vector& trans) : translation(trans) {}
+
+        /// @brief Reset translation to identity translation (i.e. no translation)
+        void identity()
+        {
+            translation = {0.0, 0.0};
+        }
+
+        /// @brief Reset the translation to a new translation quantity
+        void reset(const Vector& trans)
+        {
+            translation = trans;
+        }
+
+        /// @brief Get the current defined translation vector.
+        const Vector& vector() const
+        {
+            return translation;
+        }
+
+        /// @brief Compose two translation objects.
+        Translation compose(const Translation& trans) const
+        {
+            return Translation(translation + trans.translation);
+        }
+
+        /// @brief Apply the translation to a point in Cartesian coordinate system
+        Point apply(const Point& pnt) const
+        {
+            return pnt + translation;
+        }
+
+        /// @brief Apply the translation to a vector in Cartesian coordinate system
+        Vector apply(const Vector& vec) const
+        {
+            return vec + translation;
+        }
+
+    private:
+        /// @brief The translation values
+        Vector translation{0.0, 0.0};
+    };
+
+    /// @brief Apply a rotation transformation to a point or a vector.
+    class Rotation
+    {
+    public:
+        /// @brief Default constructor, default is theta = 0.
+        Rotation() = default;
+
+        /// @brief Construct with user defined rotation angle, in radians.
+        explicit Rotation(const double angle) : theta(angle), cosTheta(std::cos(angle)), sinTheta(std::sin(angle)) {}
+
+        /// @brief Reset rotation to identity translation (i.e. no rotation, theta = 0)
+        void identity()
+        {
+            theta = 0.0;
+            cosTheta = 1.0;
+            sinTheta = 0.0;
+        }
+
+        /// @brief Reset the rotation to a new rotation angle
+        void reset(const double angle)
+        {
+            theta = angle;
+            cosTheta = std::cos(theta);
+            sinTheta = std::sin(theta);
+        }
+
+        /// @brief Get the current defined rotation angle
+        double angle() const
+        {
+            return theta;
+        }
+
+        /// @brief Compose two rotation objects.
+        Rotation compose(const Rotation& rot) const
+        {
+            return Rotation(theta + rot.theta);
+        }
+
+        /// @brief Compose rotation and translation object.
+        ///
+        /// Will be applied rot (trans).
+        Translation compose(const Translation& trans) const
+        {
+            Vector vec(apply(trans.vector()));
+            return Translation(vec);
+        }
+
+        /// @brief Apply the rotation to a point in Cartesian coordinate system
+        Point apply(const Point& pnt) const
+        {
+            Point result({cosTheta * pnt.x - sinTheta * pnt.y, sinTheta * pnt.x + cosTheta * pnt.y});
+            return result;
+        }
+
+        /// @brief Apply the rotation to a vector in Cartesian coordinate system
+        Vector apply(const Vector& vec) const
+        {
+            Vector result({cosTheta * vec.x() - sinTheta * vec.y(),
+                           sinTheta * vec.x() + cosTheta * vec.y()});
+            return result;
+        }
+
+    private:
+        /// @brief The rotation angle, theta
+        double theta = 0.0;
+        /// @brief cosine of the rotation angle
+        double cosTheta = 1.0;
+        /// @brief sine of the rotation angle
+        double sinTheta = 0.0;
+    };
+
+    /// @brief A composition of translation and rotation transformations
+    class RigidBodyTransformation
+    {
+    public:
+        /// @brief Default constructor, default is no transformation
+        RigidBodyTransformation() = default;
+
+        /// @brief Reset trasformation to identity transformation (i.e. no transformation)
+        void identity()
+        {
+            rotation_.identity();
+            translation_.identity();
+        }
+
+        /// @brief Compose rotation and transformation object.
+        ///
+        /// Will be applied rot (transformation).
+        void compose(const Rotation& rot)
+        {
+            rotation_ = rot.compose(rotation_);
+            translation_ = rot.compose(translation_);
+        }
+
+        /// @brief Compose translation and transformation object.
+        ///
+        /// Will be applied translation (transformation).
+        void compose(const Translation& trans)
+        {
+            translation_ = trans.compose(translation_);
+        }
+
+        /// @brief Get the current rotation.
+        const Rotation& rotation() const
+        {
+            return rotation_;
+        }
+
+        /// @brief Get the current translation.
+        const Translation& translation() const
+        {
+            return translation_;
+        }
+
+        /// @brief Apply the transformation to a point in Cartesian coordinate system
+        Point apply(const Point& pnt) const
+        {
+            Point result = rotation_.apply(pnt);
+            result = translation_.apply(result);
+            return result;
+        }
+
+        /// @brief Apply the transformation to a vector in Cartesian coordinate system
+        Vector apply(const Vector& vec) const
+        {
+            Vector result = rotation_.apply(vec);
+            result = translation_.apply(result);
+            return result;
+        }
+
+    private:
+        /// @brief The rotation part of the transformation
+        Rotation rotation_;
+        /// @brief The translation part of the transformation
+        Translation translation_;
+    };
+
+    /// @brief Apply a transformation to a mesh
+    class MeshTransformation
+    {
+    public:
+        /// @brief Apply a transformation to a mesh
+        template <TransformationOperation Transformation>
+        static void Compute(Mesh& mesh, Transformation transformation);
+    };
+
+} // namespace meshkernel
+
+template <meshkernel::TransformationOperation Transformation>
+void meshkernel::MeshTransformation::Compute(Mesh& mesh, Transformation transformation)
+{
+
+    for (UInt i = 0; i < mesh.GetNumNodes(); ++i)
+    {
+        if (mesh.m_nodes[i].IsValid())
+        {
+            mesh.m_nodes[i] = transformation.apply(mesh.m_nodes[i]);
+        }
+    }
+}
---
+++ libs/MeshKernel/tests/src/MeshTransformationTest.cpp
@@ -0,0 +1,223 @@
+#include <chrono>
+#include <gtest/gtest.h>
+#include <random>
+
+#include "MeshKernel/Constants.hpp"
+#include "MeshKernel/Entities.hpp"
+#include "MeshKernel/Mesh2D.hpp"
+#include "MeshKernel/MeshTransformation.hpp"
+#include "MeshKernel/Polygons.hpp"
+
+#include "TestUtils/Definitions.hpp"
+#include "TestUtils/MakeMeshes.hpp"
+
+namespace mk = meshkernel;
+
+TEST(MeshTransformationTest, BasicTranslationTest)
+{
+    // Test basic functionality of the translation class
+    double translationX = 1.0;
+    double translationY = 2.0;
+
+    mk::Vector vec(translationX, translationY);
+    mk::Translation translation(vec);
+
+    EXPECT_EQ(translationX, translation.vector().x());
+    EXPECT_EQ(translationY, translation.vector().y());
+
+    translation.identity();
+    EXPECT_EQ(0.0, translation.vector().x());
+    EXPECT_EQ(0.0, translation.vector().y());
+
+    std::swap(vec.x(), vec.y());
+    vec.x() = -vec.x();
+    vec.y() = -vec.y();
+    translation.reset(vec);
+
+    EXPECT_EQ(-translationY, translation.vector().x());
+    EXPECT_EQ(-translationX, translation.vector().y());
+}
+
+TEST(MeshTransformationTest, BasicRotationTest)
+{
+    // Test basic functionality of the rotation class
+    double theta = 45.0 * M_PI / 180.0;
+
+    mk::Rotation rotation(theta);
+
+    EXPECT_EQ(theta, rotation.angle());
+
+    theta *= 1.5;
+    rotation.reset(theta);
+    EXPECT_EQ(theta, rotation.angle());
+
+    rotation.identity();
+    EXPECT_EQ(0.0, rotation.angle());
+}
+
+TEST(MeshTransformationTest, PointTranslationTest)
+{
+    // Test a simple translation of a point in Cartesian coordinates.
+    double originX = 12.0;
+    double originY = -19.0;
+    double translationX = 1.0;
+    double translationY = 2.0;
+
+    mk::Vector vec(translationX, translationY);
+    mk::Translation translation(vec);
+    mk::Point pnt(originX, originY);
+    pnt = translation.apply(pnt);
+
+    EXPECT_EQ(originX + translationX, pnt.x);
+    EXPECT_EQ(originY + translationY, pnt.y);
+}
+
+TEST(MeshTransformationTest, BasicRigidBodyTransformationTest)
+{
+    // Test the rigid body transformation
+    // The test will rotate a series of points about a point that is not the origin
+    //
+    // Step 1: create composition of all simple transformations
+    //    1. translate the rotation point (-1,1) to origin (0,0)
+    //    2. rotate by 90 degrees
+    //    3. translate back to original rotation point (-1,1)
+    //
+    // Step 2 apply composite transformation to a series of points
+    constexpr double tolerance = 1.0e-10;
+
+    double theta = -90.0 * M_PI / 180.0;
+    mk::Point rotationPoint(-1.0, 1.0);
+
+    mk::RigidBodyTransformation transformation;
+
+    // Expect initial state to be the identity
+    EXPECT_EQ(0.0, transformation.rotation().angle());
+    EXPECT_EQ(0.0, transformation.translation().vector().x());
+    EXPECT_EQ(0.0, transformation.translation().vector().y());
+
+    // First translate
+    mk::Vector vec(-rotationPoint.x, -rotationPoint.y);
+    mk::Translation translation(vec);
+    transformation.compose(translation);
+
+    EXPECT_EQ(0.0, transformation.rotation().angle());
+    EXPECT_EQ(-rotationPoint.x, transformation.translation().vector().x());
+    EXPECT_EQ(-rotationPoint.y, transformation.translation().vector().y());
+
+    // Second rotate
+    mk::Rotation rotation(theta);
+    transformation.compose(rotation);
+
+    mk::Vector rotatedVector = rotation.apply(vec);
+
+    EXPECT_EQ(theta, transformation.rotation().angle());
+    EXPECT_EQ(rotatedVector.x(), transformation.translation().vector().x());
+    EXPECT_EQ(rotatedVector.y(), transformation.translation().vector().y());
+
+    // Third rotate back
+    vec = {rotationPoint.x, rotationPoint.y};
+    translation.reset(vec);
+    transformation.compose(translation);
+
+    EXPECT_EQ(theta, transformation.rotation().angle());
+
+    double expectedTranslationX = -2.0;
+    double expectedTranslationY = 0.0;
+
+    EXPECT_NEAR(expectedTranslationX, transformation.translation().vector().x(), tolerance);
+    EXPECT_NEAR(expectedTranslationY, transformation.translation().vector().y(), tolerance);
+
+    //------------------------------------------------------------
+    // Apply the composite transformation to a series of points
+
+    mk::Point pnt1{-3.0, 4.0};
+    mk::Point pnt2{-2.0, 2.0};
+    mk::Point pnt3{-3.0, 1.0};
+
+    mk::Point expected1{2.0, 3.0};
+    mk::Point expected2{0.0, 2.0};
+    mk::Point expected3{-1.0, 3.0};
+
+    pnt1 = transformation.apply(pnt1);
+    EXPECT_NEAR(expected1.x, pnt1.x, tolerance);
+    EXPECT_NEAR(expected1.y, pnt1.y, tolerance);
+
+    pnt2 = transformation.apply(pnt2);
+    EXPECT_NEAR(expected2.x, pnt2.x, tolerance);
+    EXPECT_NEAR(expected2.y, pnt2.y, tolerance);
+
+    pnt3 = transformation.apply(pnt3);
+    EXPECT_NEAR(expected3.x, pnt3.x, tolerance);
+    EXPECT_NEAR(expected3.y, pnt3.y, tolerance);
+}
+
+TEST(MeshTransformationTest, PointRotationTest)
+{
+    // Test a simple rotation of a point in Cartesian coordinates.
+    double originX = 12.0;
+    double originY = -19.0;
+
+    double theta = 45.0 * M_PI / 180.0;
+    double cosTheta = std::cos(theta);
+    double sinTheta = std::sin(theta);
+
+    mk::Rotation rotation(theta);
+    mk::Point pnt(originX, originY);
+    pnt = rotation.apply(pnt);
+
+    EXPECT_EQ(originX * cosTheta - originY * sinTheta, pnt.x);
+    EXPECT_EQ(originX * sinTheta + originY * cosTheta, pnt.y);
+}
+
+TEST(MeshTransformationTest, MeshTranslationTest)
+{
+
+    mk::UInt nx = 11;
+    mk::UInt ny = 11;
+
+    double delta = 10.0;
+
+    // Make two copies of the same mesh so that they can be compared in the test.
+    std::shared_ptr<mk::Mesh2D> originalMesh = MakeRectangularMeshForTesting(nx, ny, delta, mk::Projection::cartesian);
+    std::shared_ptr<mk::Mesh2D> mesh = MakeRectangularMeshForTesting(nx, ny, delta, mk::Projection::cartesian);
+
+    mk::Vector vec(1.0, 2.0);
+
+    mk::Translation translation(vec);
+
+    mk::MeshTransformation::Compute(*mesh, translation);
+
+    for (mk::UInt i = 0; i < mesh->GetNumNodes(); ++i)
+    {
+        EXPECT_EQ(originalMesh->m_nodes[i].x + vec.x(), mesh->m_nodes[i].x);
+        EXPECT_EQ(originalMesh->m_nodes[i].y + vec.y(), mesh->m_nodes[i].y);
+    }
+}
+
+TEST(MeshTransformationTest, MeshRotationTest)
+{
+
+    mk::UInt nx = 11;
+    mk::UInt ny = 11;
+
+    double delta = 10.0;
+
+    // Make two copies of the same mesh so that they can be compared in the test.
+    std::shared_ptr<mk::Mesh2D> originalMesh = MakeRectangularMeshForTesting(nx, ny, delta, mk::Projection::cartesian);
+    std::shared_ptr<mk::Mesh2D> mesh = MakeRectangularMeshForTesting(nx, ny, delta, mk::Projection::cartesian);
+
+    double theta = 45.0 * M_PI / 180.0;
+
+    mk::Rotation rotation(theta);
+    double cosTheta = std::cos(theta);
+    double sinTheta = std::sin(theta);
+
+    mk::MeshTransformation::Compute(*mesh, rotation);
+
+    for (mk::UInt i = 0; i < mesh->GetNumNodes(); ++i)
+    {
+        mk::Point expected{originalMesh->m_nodes[i].x * cosTheta - originalMesh->m_nodes[i].y * sinTheta, originalMesh->m_nodes[i].x * sinTheta + originalMesh->m_nodes[i].y * cosTheta};
+        EXPECT_EQ(expected.x, mesh->m_nodes[i].x);
+        EXPECT_EQ(expected.y, mesh->m_nodes[i].y);
+    }
+}
