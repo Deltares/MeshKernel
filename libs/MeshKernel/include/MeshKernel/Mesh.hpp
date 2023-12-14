b50907c8524e0bbabcb919386db5120c8efde221
Luca Carniato <luca.carniato@deltares.nl>
2023-12-14 15:10:41 +0100

    Fix macos build

--- libs/MeshKernel/include/MeshKernel/Mesh.hpp
+++ libs/MeshKernel/include/MeshKernel/Mesh.hpp
@@ -26,6 +26,7 @@
 //------------------------------------------------------------------------------
 
 #pragma once
+#include <memory>
 
 #include "MeshKernel/BoundingBox.hpp"
 #include "MeshKernel/Definitions.hpp"
