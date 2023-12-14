aa77c8d2f7d341375bd2a3d3e05f4221efba3942
Luca Carniato <luca.carniato@deltares.nl>
2023-12-14 14:18:59 +0100

    Refactor RTree.hpp

--- libs/MeshKernel/include/MeshKernel/Utilities/RTree.hpp
+++ libs/MeshKernel/include/MeshKernel/Utilities/RTree.hpp
@@ -66,129 +66,57 @@ namespace meshkernel
     {
         using Point2D = bg::model::point<double, 2, projection>; ///< Typedef for Point2D
         using Box2D = bg::model::box<Point2D>;                   ///< Typedef for box of Point2D
-        using Value2D = std::pair<Point2D, UInt>;                ///< Typedef of pair of Point2D and size_t
+        using Value2D = std::pair<Point2D, UInt>;                ///< Typedef of pair of Point2D and UInt
         using RTree2D = bgi::rtree<Value2D, bgi::linear<16>>;    ///< Typedef for a 2D RTree
 
     public:
+        /// @brief Builds the tree from a vector of Points
+        /// @param[in] nodes The vector of nodes
         void BuildTree(const std::vector<Point>& nodes) override
         {
             BuildTreeFromVector(nodes);
         }
 
+        /// @brief Builds the tree from a vector of samples
+        /// @param[in] samples The vector of samples
         void BuildTree(const std::vector<Sample>& samples) override
         {
             BuildTreeFromVector(samples);
         }
 
+        /// @brief Builds the tree from a vector of points within a bounding box
+        /// @param[in] nodes The vector of nodes
+        /// @param[in] boundingBox The vector bounding box
         void BuildTree(const std::vector<Point>& nodes, const BoundingBox& boundingBox) override
         {
-            BuildTreeFromVectorWithBoundingBox(nodes, boundingBox);
+            BuildTreeFromVectorWithinBoundingBox(nodes, boundingBox);
         }
 
+        /// @brief Builds the tree from a vector of samples within a bounding box
+        /// @param[in] samples The vector of samples
+        /// @param[in] boundingBox The vector bounding box
         void BuildTree(const std::vector<Sample>& samples, const BoundingBox& boundingBox) override
         {
-            BuildTreeFromVectorWithBoundingBox(samples, boundingBox);
+            BuildTreeFromVectorWithinBoundingBox(samples, boundingBox);
         }
 
         /// @brief Finds all nodes in the search radius and stores the results in the query cache, to be inquired later
         /// @param[in] node The node
         /// @param[in] searchRadiusSquared The squared search radius around the node
-        void SearchPoints(Point const& node, double searchRadiusSquared) override
-        {
-            if (Empty())
-            {
-                throw AlgorithmError("RTree is empty, search cannot be performed");
-            }
-
-            const auto searchRadius = std::sqrt(searchRadiusSquared);
-
-            Box2D const box(Point2D(node.x - searchRadius, node.y - searchRadius),
-                            Point2D(node.x + searchRadius, node.y + searchRadius));
-            Point2D nodeSought = Point2D(node.x, node.y);
-
-            m_queryCache.reserve(m_queryVectorCapacity);
-            m_queryCache.clear();
-            m_rtree2D.query(bgi::within(box) &&
-                                bgi::satisfies([&nodeSought, &searchRadiusSquared](Value2D const& v)
-                                               { return bg::comparable_distance(v.first, nodeSought) <= searchRadiusSquared; }),
-                            std::back_inserter(m_queryCache));
-
-            m_queryIndices.reserve(m_queryCache.size());
-            m_queryIndices.clear();
-
-            for (size_t i = 0; i < m_queryCache.size(); ++i)
-            {
-                auto const index = std::get<1>(m_queryCache[i]);
-                m_queryIndices.emplace_back(index);
-            }
-        }
+        void SearchPoints(Point const& node, double searchRadiusSquared) override;
 
         /// @brief Finds the nearest node in the search radius and stores the results in the query cache, to be inquired later
         /// @param[in] node The node
         /// @param[in] searchRadiusSquared The squared search radius around the node
-        void SearchNearestPoint(Point const& node, double searchRadiusSquared) override
-        {
-            if (Empty())
-            {
-                throw AlgorithmError("RTree is empty, search cannot be performed");
-            }
-
-            m_queryCache.reserve(m_queryVectorCapacity);
-            m_queryCache.clear();
-            const Point2D nodeSought = Point2D(node.x, node.y);
-            const auto searchRadius = std::sqrt(searchRadiusSquared);
-            Box2D const box(Point2D(node.x - searchRadius, node.y - searchRadius),
-                            Point2D(node.x + searchRadius, node.y + searchRadius));
-            m_rtree2D.query(bgi::within(box) &&
-                                bgi::satisfies([&nodeSought, &searchRadiusSquared](Value2D const& v)
-                                               { return bg::comparable_distance(v.first, nodeSought) <= searchRadiusSquared; }) &&
-                                bgi::nearest(nodeSought, 1),
-                            std::back_inserter(m_queryCache));
-
-            if (!m_queryCache.empty())
-            {
-                m_queryIndices.clear();
-                m_queryIndices.emplace_back(m_queryCache[0].second);
-            }
-        }
+        void SearchNearestPoint(Point const& node, double searchRadiusSquared) override;
 
         /// @brief Gets the nearest of all nodes
         /// @param[in] node The node
-        void SearchNearestPoint(Point const& node) override
-        {
-            if (Empty())
-            {
-                throw AlgorithmError("RTree is empty, search cannot be performed");
-            }
-
-            m_queryCache.reserve(m_queryVectorCapacity);
-            m_queryCache.clear();
-            const Point2D nodeSought = Point2D(node.x, node.y);
-            m_rtree2D.query(bgi::nearest(nodeSought, 1), std::back_inserter(m_queryCache));
-
-            if (!m_queryCache.empty())
-            {
-                m_queryIndices.clear();
-                m_queryIndices.emplace_back(m_queryCache[0].second);
-            }
-        }
+        void SearchNearestPoint(Point const& node) override;
 
         /// @brief Deletes a node
         /// @param[in] position The index of the point to remove in m_points
-        void DeleteNode(UInt position) override
-        {
-            if (Empty())
-            {
-                throw AlgorithmError("RTree is empty, deletion cannot performed");
-            }
-
-            const auto numberRemoved = m_rtree2D.remove(m_points[position]);
-            if (numberRemoved != 1)
-            {
-                return;
-            }
-            m_points[position] = {Point2D{constants::missing::doubleValue, constants::missing::doubleValue}, std::numeric_limits<UInt>::max()};
-        }
+        void DeleteNode(UInt position) override;
 
         /// @brief Determines size of the RTree
         [[nodiscard]] UInt Size() const override { return static_cast<UInt>(m_rtree2D.size()); };
@@ -206,12 +134,10 @@ namespace meshkernel
         [[nodiscard]] bool HasQueryResults() const override { return !m_queryCache.empty(); }
 
     private:
-        /// @brief Builds the tree
-        /// @param[in] nodes The nodes
+        /// @brief Builds the tree from a vector of types derived from Point
         template <std::derived_from<Point> T>
         void BuildTreeFromVector(const std::vector<T>& nodes)
         {
-            m_points.reserve(m_points.size());
             m_points.clear();
             m_rtree2D.clear();
 
@@ -225,12 +151,10 @@ namespace meshkernel
             m_rtree2D = RTree2D(m_points);
         }
 
-        /// @brief Builds the tree with nodes
-        /// @param[in] nodes The nodes
+        /// @brief Builds the tree from a vector of types derived from Point within a bounding box
         template <std::derived_from<Point> T>
-        void BuildTreeFromVectorWithBoundingBox(const std::vector<T>& nodes, const BoundingBox& boundingBox)
+        void BuildTreeFromVectorWithinBoundingBox(const std::vector<T>& nodes, const BoundingBox& boundingBox)
         {
-            m_points.reserve(m_points.size());
             m_points.clear();
             m_rtree2D.clear();
 
@@ -255,4 +179,99 @@ namespace meshkernel
         std::vector<UInt> m_queryIndices;               ///< The query indices
         UInt m_queryVectorCapacity = 100;               ///< Capacity of the query vector
     };
+
+    template <typename projection>
+    void RTree<projection>::SearchPoints(Point const& node, double searchRadiusSquared)
+    {
+        if (Empty())
+        {
+            throw AlgorithmError("RTree is empty, search cannot be performed");
+        }
+
+        const auto searchRadius = std::sqrt(searchRadiusSquared);
+
+        Box2D const box(Point2D(node.x - searchRadius, node.y - searchRadius),
+                        Point2D(node.x + searchRadius, node.y + searchRadius));
+        Point2D nodeSought = Point2D(node.x, node.y);
+
+        m_queryCache.reserve(m_queryVectorCapacity);
+        m_queryCache.clear();
+        m_rtree2D.query(bgi::within(box) &&
+                            bgi::satisfies([&nodeSought, &searchRadiusSquared](Value2D const& v)
+                                           { return bg::comparable_distance(v.first, nodeSought) <= searchRadiusSquared; }),
+                        std::back_inserter(m_queryCache));
+
+        m_queryIndices.reserve(m_queryCache.size());
+        m_queryIndices.clear();
+
+        for (size_t i = 0; i < m_queryCache.size(); ++i)
+        {
+            auto const index = std::get<1>(m_queryCache[i]);
+            m_queryIndices.emplace_back(index);
+        }
+    }
+
+    template <typename projection>
+    void RTree<projection>::SearchNearestPoint(Point const& node, double searchRadiusSquared)
+    {
+        if (Empty())
+        {
+            throw AlgorithmError("RTree is empty, search cannot be performed");
+        }
+
+        m_queryCache.reserve(m_queryVectorCapacity);
+        m_queryCache.clear();
+        const Point2D nodeSought = Point2D(node.x, node.y);
+        const auto searchRadius = std::sqrt(searchRadiusSquared);
+        Box2D const box(Point2D(node.x - searchRadius, node.y - searchRadius),
+                        Point2D(node.x + searchRadius, node.y + searchRadius));
+        m_rtree2D.query(bgi::within(box) &&
+                            bgi::satisfies([&nodeSought, &searchRadiusSquared](Value2D const& v)
+                                           { return bg::comparable_distance(v.first, nodeSought) <= searchRadiusSquared; }) &&
+                            bgi::nearest(nodeSought, 1),
+                        std::back_inserter(m_queryCache));
+
+        if (!m_queryCache.empty())
+        {
+            m_queryIndices.clear();
+            m_queryIndices.emplace_back(m_queryCache[0].second);
+        }
+    }
+
+    template <typename projection>
+    void RTree<projection>::SearchNearestPoint(Point const& node)
+    {
+        if (Empty())
+        {
+            throw AlgorithmError("RTree is empty, search cannot be performed");
+        }
+
+        m_queryCache.reserve(m_queryVectorCapacity);
+        m_queryCache.clear();
+        const Point2D nodeSought = Point2D(node.x, node.y);
+        m_rtree2D.query(bgi::nearest(nodeSought, 1), std::back_inserter(m_queryCache));
+
+        if (!m_queryCache.empty())
+        {
+            m_queryIndices.clear();
+            m_queryIndices.emplace_back(m_queryCache[0].second);
+        }
+    }
+
+    template <typename projection>
+    void RTree<projection>::DeleteNode(UInt position)
+    {
+        if (Empty())
+        {
+            throw AlgorithmError("RTree is empty, deletion cannot performed");
+        }
+
+        const auto numberRemoved = m_rtree2D.remove(m_points[position]);
+        if (numberRemoved != 1)
+        {
+            return;
+        }
+        m_points[position] = {Point2D{constants::missing::doubleValue, constants::missing::doubleValue}, std::numeric_limits<UInt>::max()};
+    }
+
 } // namespace meshkernel
