405afd581a00c69510cdee8b62f863bf4eb4d763
BillSenior <bill.senior@deltares.nl>
2023-12-18 15:58:57 +0100

    GRIDEDIT-815 Added smoothness calculation for curvilinear grids

---
+++ libs/MeshKernel/include/MeshKernel/CurvilinearGrid/CurvilinearGridSmoothness.hpp
@@ -0,0 +1,55 @@
+//---- GPL ---------------------------------------------------------------------
+//
+// Copyright (C)  Stichting Deltares, 2011-2023.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// contact: delft3d.support@deltares.nl
+// Stichting Deltares
+// P.O. Box 177
+// 2600 MH Delft, The Netherlands
+//
+// All indications and logos of, and references to, "Delft3D" and "Deltares"
+// are registered trademarks of Stichting Deltares, and remain the property of
+// Stichting Deltares. All rights reserved.
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "MeshKernel/Utilities/LinearAlgebra.hpp"
+
+#include "MeshKernel/Point.hpp"
+
+#include "MeshKernel/CurvilinearGrid/CurvilinearGrid.hpp"
+
+namespace meshkernel
+{
+
+    /// @brief Compute the smoothness of a grid.
+    class CurvilinearGridSmoothness
+    {
+    public:
+        /// @brief Compute the smoothness of the grid in the direction specified.
+        ///
+        /// @param grid Grid for which the smoothness is to be calculated.
+        /// @param direction Direction of the smoothness required.
+        /// @param smoothness Matrix of smoothness values, will be resized to the size of the grid.
+        static void Compute(const CurvilinearGrid& grid, const int direction, lin_alg::Matrix<double>& smoothness);
+
+    private:
+        /// @brief Compute the smoothness for three points.
+        static double ComputeSmoothness(Point p0, Point p1, Point p2);
+    };
+
+} // namespace meshkernel
---
+++ libs/MeshKernel/src/CurvilinearGrid/CurvilinearGridSmoothness.cpp
@@ -0,0 +1,78 @@
+#include <cmath>
+
+#include "MeshKernel/Constants.hpp"
+#include "MeshKernel/Vector.hpp"
+
+#include "MeshKernel/CurvilinearGrid/CurvilinearGridSmoothness.hpp"
+
+void meshkernel::CurvilinearGridSmoothness::Compute(const CurvilinearGrid& grid, const int direction, lin_alg::Matrix<double>& smoothness)
+{
+
+    smoothness.resize(grid.m_numM, grid.m_numN);
+    smoothness.fill(constants::missing::doubleValue);
+
+    if (direction == 1)
+    {
+        for (UInt i = 1; i < grid.m_numM - 1; ++i)
+        {
+            for (UInt j = 0; j < grid.m_numN; ++j)
+            {
+                smoothness(i, j) = ComputeSmoothness(grid.m_gridNodes(i - 1, j), grid.m_gridNodes(i, j), grid.m_gridNodes(i + 1, j));
+            }
+        }
+    }
+    else
+    {
+        for (UInt i = 0; i < grid.m_numM; ++i)
+        {
+            for (UInt j = 1; j < grid.m_numN - 1; ++j)
+            {
+                smoothness(i, j) = ComputeSmoothness(grid.m_gridNodes(i, j - 1), grid.m_gridNodes(i, j), grid.m_gridNodes(i, j + 1));
+            }
+        }
+    }
+}
+
+double meshkernel::CurvilinearGridSmoothness::ComputeSmoothness(Point p0, Point p1, Point p2)
+{
+
+    bool isValid = true;
+
+    if (!p0.IsValid())
+    {
+        isValid = false;
+        // p0 = p1;
+    }
+
+    if (!p2.IsValid())
+    {
+        isValid = false;
+        // p2 = p1;
+    }
+
+    double nodeSmoothness = constants::missing::doubleValue;
+
+    if (p1.IsValid() && isValid)
+    {
+        double diffX21 = p2.x - p1.x;
+        double diffY21 = p2.y - p1.y;
+
+        double diffX10 = p1.x - p0.x;
+        double diffY10 = p1.y - p0.y;
+
+        double lengthSquared21 = diffX21 * diffX21 + diffY21 * diffY21;
+        double lengthSquared10 = diffX10 * diffX10 + diffY10 * diffY10;
+
+        if (lengthSquared10 != 0.0 && lengthSquared21 != 0.0)
+        {
+            nodeSmoothness = std::sqrt(lengthSquared21 / lengthSquared10);
+
+            if (nodeSmoothness < 1.0)
+            {
+                nodeSmoothness = 1.0 / nodeSmoothness;
+            }
+        }
+    }
+
+    return nodeSmoothness;
+}
