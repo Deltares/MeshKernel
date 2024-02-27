47f114905cef6660674b61d1427d204bba486e23
BillSenior <bill.senior@deltares.nl>
2024-02-27 16:33:06 +0100

    GRIDEDIT-899 Added unit test for merging points in polygon and undo

--- libs/MeshKernel/tests/src/CompoundUndoTests.cpp
+++ libs/MeshKernel/tests/src/CompoundUndoTests.cpp
@@ -10,6 +10,7 @@
 #include "MeshKernel/ResetEdgeAction.hpp"
 #include "MeshKernel/ResetNodeAction.hpp"
 #include "MeshKernel/UndoActionStack.hpp"
+#include "MeshKernel/Polygons.hpp"
 
 #include "MeshKernel/Constants.hpp"
 #include "MeshKernel/Entities.hpp"
@@ -20,12 +21,10 @@
 
 namespace mk = meshkernel;
 
-TEST(CompoundUndoTests, ResetNodeInMesh)
+TEST(CompoundUndoTests, MergeTwoNodesInMesh)
 {
     auto mesh = MakeRectangularMeshForTesting(3, 3, 1.0, meshkernel::Projection::cartesian);
     mk::UndoActionStack undoActionStack;
-
-    std::vector<mk::Point> originalNodes(mesh->Nodes());
 
     mk::Point node(1.5, 1.5);
 
@@ -72,4 +71,44 @@ TEST(CompoundUndoTests, ResetNodeInMesh)
 
     EXPECT_EQ(mesh->Node(9).x, node.x);
     EXPECT_EQ(mesh->Node(9).y, node.y);
+}
+
+TEST(CompoundUndoTests, MergeNodesInPolygonInMesh)
+{
+    auto mesh = MakeRectangularMeshForTesting(11, 11, 1.0, meshkernel::Projection::cartesian);
+    const std::vector<mk::Point> originalNodes (mesh->Nodes ());
+    const std::vector<mk::Edge> originalEdges (mesh->Edges ());
+
+    mk::UndoActionStack undoActionStack;
+
+    std::vector<mk::Point> polygonNodes{{3.5, 3.5}, {6.5, 3.5}, {6.5, 6.5}, {3.5, 6.5}, {3.5, 3.5}};
+    mk::Polygons polygons (polygonNodes, mesh->m_projection);
+
+    EXPECT_EQ (mesh->GetNumNodes (), originalNodes.size ());
+    EXPECT_EQ (mesh->GetNumValidNodes (), originalNodes.size ());
+    EXPECT_EQ (mesh->GetNumEdges (), originalEdges.size ());
+    EXPECT_EQ (mesh->GetNumValidEdges (), originalEdges.size ());
+
+    undoActionStack.Add (mesh->MergeNodesInPolygon (polygons, 1.5));
+
+    EXPECT_EQ (mesh->GetNumNodes (), originalNodes.size ());
+    EXPECT_EQ (mesh->GetNumValidNodes (), 113);
+    EXPECT_EQ (mesh->GetNumEdges (), originalEdges.size ());
+    EXPECT_EQ (mesh->GetNumValidEdges (), 208);
+
+    // Undoing merge should restore the original mesh.
+    undoActionStack.Undo ();
+
+    for (size_t i = 0; i < originalNodes.size (); ++i)
+    {
+        EXPECT_EQ (originalNodes[i].x, mesh->Node (i).x);
+        EXPECT_EQ (originalNodes[i].y, mesh->Node (i).y);
+    }
+
+    for (size_t i = 0; i < originalEdges.size (); ++i)
+    {
+        EXPECT_EQ (originalEdges[i].first, mesh->GetEdge (i).first);
+        EXPECT_EQ (originalEdges[i].second, mesh->GetEdge (i).second);
+    }
+
 }
