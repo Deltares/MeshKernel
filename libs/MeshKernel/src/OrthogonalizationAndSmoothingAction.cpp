d31fb85b8c00c5413d638ff5fd76f362d85cd8ac
BillSenior <bill.senior@deltares.nl>
2024-03-05 15:38:13 +0100

    GRIDEDIT-899 Fixed clang formatting warning

--- libs/MeshKernel/src/OrthogonalizationAndSmoothingAction.cpp
+++ libs/MeshKernel/src/OrthogonalizationAndSmoothingAction.cpp
@@ -1,18 +1,17 @@
 #include "MeshKernel/OrthogonalizationAndSmoothingAction.hpp"
 #include "MeshKernel/Mesh2D.hpp"
 
-
 std::unique_ptr<meshkernel::OrthogonalizationAndSmoothingAction> meshkernel::OrthogonalizationAndSmoothingAction::Create(Mesh2D& mesh, const std::vector<Point>& nodes)
 {
-    return std::make_unique<OrthogonalizationAndSmoothingAction> (mesh, nodes);
+    return std::make_unique<OrthogonalizationAndSmoothingAction>(mesh, nodes);
 }
 
 meshkernel::OrthogonalizationAndSmoothingAction::OrthogonalizationAndSmoothingAction(Mesh2D& mesh, const std::vector<Point>& nodes)
-    : BaseMeshUndoAction<OrthogonalizationAndSmoothingAction, Mesh2D> (mesh), m_nodes (nodes){}
+    : BaseMeshUndoAction<OrthogonalizationAndSmoothingAction, Mesh2D>(mesh), m_nodes(nodes) {}
 
 void meshkernel::OrthogonalizationAndSmoothingAction::Swap(std::vector<Point>& nodes)
 {
-    std::swap_ranges(m_nodes.begin (), m_nodes.end (), nodes.begin ());
+    std::swap_ranges(m_nodes.begin(), m_nodes.end(), nodes.begin());
 }
 
 std::uint64_t meshkernel::OrthogonalizationAndSmoothingAction::MemorySize() const
