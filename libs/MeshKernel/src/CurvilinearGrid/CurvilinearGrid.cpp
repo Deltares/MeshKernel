210beef26fb5416824f26d1131d2ba6e827bc380
Luca Carniato <luca.carniato@deltares.nl>
2024-05-03 16:52:57 +0200

    Simplify node comparision

--- libs/MeshKernel/src/CurvilinearGrid/CurvilinearGrid.cpp
+++ libs/MeshKernel/src/CurvilinearGrid/CurvilinearGrid.cpp
@@ -717,25 +717,19 @@ CurvilinearGrid::BoundaryGridLineType CurvilinearGrid::GetBoundaryGridLineType(C
         throw std::invalid_argument("CurvilinearGrid::GetBoundaryGridLineType: Not a boundary grid line");
     }
 
-    if (firstNodeType == NodeType::Bottom || secondNodeType == NodeType::Bottom ||
-        (firstNodeType == NodeType::BottomLeft && secondNodeType == NodeType::BottomRight))
+    if (firstNodeType == NodeType::Bottom && secondNodeType == NodeType::Bottom)
     {
         return BoundaryGridLineType::Left;
     }
-    if (firstNodeType == NodeType::Up || secondNodeType == NodeType::Up ||
-        (firstNodeType == NodeType::UpperLeft && secondNodeType == NodeType::UpperRight) ||
-        (firstNodeType == NodeType::UpperRight && secondNodeType == NodeType::UpperLeft))
+    if (firstNodeType == NodeType::Up && secondNodeType == NodeType::Up)
     {
         return BoundaryGridLineType::Right;
     }
-    if (firstNodeType == NodeType::Left || secondNodeType == NodeType::Left ||
-        (firstNodeType == NodeType::BottomLeft && secondNodeType == NodeType::UpperLeft) ||
-        (firstNodeType == NodeType::UpperLeft && secondNodeType == NodeType::BottomLeft))
+    if (firstNodeType == NodeType::Left && secondNodeType == NodeType::Left)
     {
         return BoundaryGridLineType::Bottom;
     }
-    if (firstNodeType == NodeType::Right || secondNodeType == NodeType::Right ||
-        (firstNodeType == NodeType::BottomRight && secondNodeType == NodeType::UpperRight))
+    if (firstNodeType == NodeType::Right && secondNodeType == NodeType::Right)
     {
         return BoundaryGridLineType::Top;
     }
@@ -743,7 +737,7 @@ CurvilinearGrid::BoundaryGridLineType CurvilinearGrid::GetBoundaryGridLineType(C
     // Corner nodes
     if (firstNode.m_m == secondNode.m_m) // Left or Right
     {
-        if (GetNode(firstNode.m_n, firstNode.m_m + 1).IsValid() && GetNode(secondNode.m_n, secondNode.m_m + 1).IsValid())
+        if (firstNode.m_m + 1 < m_gridNodes.cols() && GetNode(firstNode.m_n, firstNode.m_m + 1).IsValid() && GetNode(secondNode.m_n, secondNode.m_m + 1).IsValid())
         {
             return BoundaryGridLineType::Left;
         }
@@ -751,7 +745,7 @@ CurvilinearGrid::BoundaryGridLineType CurvilinearGrid::GetBoundaryGridLineType(C
     }
     if (firstNode.m_n == secondNode.m_n) // Bottom or top
     {
-        if (GetNode(firstNode.m_n + 1, firstNode.m_m).IsValid() && GetNode(secondNode.m_n + 1, secondNode.m_m).IsValid())
+        if (firstNode.m_n + 1 < m_gridNodes.rows() && GetNode(firstNode.m_n + 1, firstNode.m_m).IsValid() && GetNode(secondNode.m_n + 1, secondNode.m_m).IsValid())
         {
             return BoundaryGridLineType::Bottom;
         }
