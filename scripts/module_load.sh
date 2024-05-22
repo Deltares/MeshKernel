766486333f4687d6a577a2de6cfbba60b7c3dd74
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-05-22 11:00:57 +0200

    Make module loading verbose and print debug info

--- scripts/module_load.sh
+++ scripts/module_load.sh
@@ -1,7 +1,7 @@
 # Script that is meant to be sourced in the CI
 # It includes the necessary module loads to build MeshKernel on Deltares Linux machines
 
-module load gcc/12.2.0_gcc12.2.0
-module load cmake/3.26.4_gcc12.2.0
-module load boost/1.83.0_gcc12.2.0
-module load netcdf/4.9.2_4.6.1_gcc12.2.0
+module -DD -vv load gcc/12.2.0_gcc12.2.0
+module -DD -vv load cmake/3.26.4_gcc12.2.0
+module -DD -vv load boost/1.83.0_gcc12.2.0
+module -DD -vv load netcdf/4.9.2_4.6.1_gcc12.2.0
