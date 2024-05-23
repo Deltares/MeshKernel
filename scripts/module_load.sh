42dae79d31ae936219a930e70902a057a1094545
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-05-23 10:36:23 +0200

    Use newly installed netCDF v4.9.2 that actually has CMake support

--- scripts/module_load.sh
+++ scripts/module_load.sh
@@ -1,7 +1,7 @@
 # Script that is meant to be sourced in the CI
 # It includes the necessary module loads to build MeshKernel on Deltares Linux machines
 
-module --debug --verbose load gcc/12.2.0_gcc12.2.0
-module --debug --verbose load cmake/3.26.4_gcc12.2.0
-module --debug --verbose load boost/1.83.0_gcc12.2.0
-module --debug --verbose load netcdf/4.9.2_4.6.1_gcc12.2.0
+module --verbose load gcc/12.2.0_gcc12.2.0
+module --verbose load cmake/3.26.4_gcc12.2.0
+module --verbose load boost/1.83.0_gcc12.2.0
+module --verbose load netcdf/4.9.2_gcc12.2.0
