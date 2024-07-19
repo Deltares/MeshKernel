5e55700c96e461315ca4b169ccc4bc1d48f56628
Luca Carniato <luca.carniato@deltares.nl>
2024-07-19 13:07:05 +0200

    Add unit test

--- libs/MeshKernel/tests/src/CurvilinearGridBoundaryPolygonTests.cpp
+++ libs/MeshKernel/tests/src/CurvilinearGridBoundaryPolygonTests.cpp
@@ -127,3 +127,47 @@ TEST(CurvilinearGridBoundaryPolygon, ComputeBoundaryToPolygon_OnValidCurvilinear
 
     ASSERT_THAT(boundaryPolygon, ::testing::ContainerEq(expectedBoundaryPolygon));
 }
+
+TEST(CurvilinearGridBoundaryPolygon, ComputeBoundaryToPolygon_OnValidCurvilinearGridWithDisconnectedCurvilinearGridAndSelection_ShouldComputeBoundaryPolygons)
+
+{
+    // Prepare
+    constexpr double originX = 0.0;
+    constexpr double originY = 0.0;
+
+    constexpr double deltaX = 1.0;
+    constexpr double deltaY = 1.0;
+
+    constexpr UInt nx = 9;
+    constexpr UInt ny = 3;
+
+    std::unique_ptr<CurvilinearGrid> grid = MakeCurvilinearGrid(originX, originY, deltaX, deltaY, nx, ny);
+    auto undoAction = grid->DeleteNode({3.0, 1.0});
+    undoAction = grid->DeleteNode({4.0, 1.0});
+    CurvilinearGridNodeIndices lowerLeft(0, 0);
+    CurvilinearGridNodeIndices upperRight(1, nx - 1); // not the entire clg
+
+    // Execute
+    const auto boundaryPolygon = grid->ComputeBoundaryToPolygon(lowerLeft, upperRight);
+
+    // Assert
+    std::vector<Point> expectedBoundaryPolygon{{0.0, 0.0},
+                                               {1.0, 0.0},
+                                               {2.0, 0.0},
+                                               {2.0, 1.0},
+                                               {1.0, 1.0},
+                                               {0.0, 1.0},
+                                               {0.0, 0.0},
+                                               {-999.0, -999.0},
+                                               {5.0, 0.0},
+                                               {6.0, 0.0},
+                                               {7.0, 0.0},
+                                               {8.0, 0.0},
+                                               {8.0, 1.0},
+                                               {7.0, 1.0},
+                                               {6.0, 1.0},
+                                               {5.0, 1.0},
+                                               {5.0, 0.0}};
+
+    ASSERT_THAT(boundaryPolygon, ::testing::ContainerEq(expectedBoundaryPolygon));
+}
