364a47eec1568985c405570292180f3c963f5d06
Luca Carniato <luca.carniato@deltares.nl>
2024-10-23 19:06:54 +0200

    add SetMesh2dApiDimensions call in SetMesh2dApiNodeEdgeData and SetMesh2dApiNodeEdgeFaceNodeData

--- libs/MeshKernelApi/include/MeshKernelApi/Utils.hpp
+++ libs/MeshKernelApi/include/MeshKernelApi/Utils.hpp
@@ -368,6 +368,7 @@ namespace meshkernelapi
             mesh2dApi.edge_nodes[edgeIndex * 2] = static_cast<int>(mesh2d.GetEdge(edgeIndex).first);
             mesh2dApi.edge_nodes[edgeIndex * 2 + 1] = static_cast<int>(mesh2d.GetEdge(edgeIndex).second);
         }
+        SetMesh2dApiDimensions(mesh2d, mesh2dApi);
     }
 
     /// @brief Sets only the node, edge and face-node data for meshkernelapi::Mesh2D
@@ -416,6 +417,8 @@ namespace meshkernelapi
                 faceIndex++;
             }
         }
+
+        SetMesh2dApiDimensions(mesh2d, mesh2dApi);
     }
 
     /// @brief Sets the meshkernelapi::Mesh2D data
@@ -456,7 +459,6 @@ namespace meshkernelapi
                 faceIndex++;
             }
         }
-
         SetMesh2dApiDimensions(mesh2d, mesh2dApi);
     }
 
