964c1c4136ea885c601d7ce95dbb2aa5fc3b8e1a
Bill Senior <bill.senior@deltares.nl>
2024-11-26 11:51:50 +0100

    GRIDEDIT-1536 Fixed clang formatting warnings

--- libs/MeshKernel/tests/src/MeshPropertyTests.cpp
+++ libs/MeshKernel/tests/src/MeshPropertyTests.cpp
@@ -22,48 +22,40 @@ TEST(MeshPropertyTests, TriangulationTest)
 
     mk::MeshTriangulation triangulation(xValues, yValues, mk::Projection::cartesian);
 
-    EXPECT_EQ (triangulation.GetProjection (), mk::Projection::cartesian);
-    ASSERT_EQ (triangulation.NumberOfNodes(), 16);
-    ASSERT_EQ (triangulation.NumberOfEdges(), 33);
-    ASSERT_EQ (triangulation.NumberOfFaces(), 18);
+    EXPECT_EQ(triangulation.GetProjection(), mk::Projection::cartesian);
+    ASSERT_EQ(triangulation.NumberOfNodes(), 16);
+    ASSERT_EQ(triangulation.NumberOfEdges(), 33);
+    ASSERT_EQ(triangulation.NumberOfFaces(), 18);
 
-    std::vector<mk::Edge> expectedEdgeIds{{4, 0}, {0, 1}, {1, 4}, {1, 5}, {5, 4}, {1, 2}, {2, 5}, {5, 9},
-                                          {9, 4}, {12, 8}, {8, 9}, {9, 12}, {9, 13}, {13, 12}, {9, 10},
-                                          {10, 13}, {8, 4}, {5, 6}, {6, 9}, {2, 6}, {2, 3}, {3, 6}, {3, 7},
-                                          {7, 6}, {7, 11}, {11, 6}, {10, 14}, {14, 13}, {10, 11}, {11, 14},
-                                          {11, 15}, {15, 14}, {10, 6}};
-    std::vector<std::array<mk::UInt, 3>> expectedTriangleNodeIds{{4, 0, 1}, {1, 5, 4}, {5, 1, 2},
-                                                                 {4, 5, 9}, {12, 8, 9}, {9, 13, 12},
-                                                                 {13, 9, 10}, {8, 4, 9}, {5, 6, 9},
-                                                                 {2, 6, 5}, {6, 2, 3}, {3, 7, 6},
-                                                                 {6, 7, 11}, {10, 14, 13}, {14, 10, 11},
-                                                                 {11, 15, 14}, {10, 6, 11}, {6, 10, 9}};
+    std::vector<mk::Edge> expectedEdgeIds{{4, 0}, {0, 1}, {1, 4}, {1, 5}, {5, 4}, {1, 2}, {2, 5}, {5, 9}, {9, 4}, {12, 8}, {8, 9}, {9, 12}, {9, 13}, {13, 12}, {9, 10}, {10, 13}, {8, 4}, {5, 6}, {6, 9}, {2, 6}, {2, 3}, {3, 6}, {3, 7}, {7, 6}, {7, 11}, {11, 6}, {10, 14}, {14, 13}, {10, 11}, {11, 14}, {11, 15}, {15, 14}, {10, 6}};
+    std::vector<std::array<mk::UInt, 3>> expectedTriangleNodeIds{{4, 0, 1}, {1, 5, 4}, {5, 1, 2}, {4, 5, 9}, {12, 8, 9}, {9, 13, 12}, {13, 9, 10}, {8, 4, 9}, {5, 6, 9}, {2, 6, 5}, {6, 2, 3}, {3, 7, 6}, {6, 7, 11}, {10, 14, 13}, {14, 10, 11}, {11, 15, 14}, {10, 6, 11}, {6, 10, 9}};
 
-
-    for (mk::UInt i = 0; i < triangulation.NumberOfEdges(); ++i) {
-        auto edge = triangulation.GetEdge (i);
-        EXPECT_EQ (edge.first, expectedEdgeIds [i].first);
-        EXPECT_EQ (edge.second, expectedEdgeIds [i].second);
+    for (mk::UInt i = 0; i < triangulation.NumberOfEdges(); ++i)
+    {
+        auto edge = triangulation.GetEdge(i);
+        EXPECT_EQ(edge.first, expectedEdgeIds[i].first);
+        EXPECT_EQ(edge.second, expectedEdgeIds[i].second);
     }
 
-
-    for (mk::UInt i = 0; i < triangulation.NumberOfFaces(); ++i) {
-        auto face = triangulation.GetNodeIds (i);
-        EXPECT_EQ (face [0], expectedTriangleNodeIds [i][0]);
-        EXPECT_EQ (face [1], expectedTriangleNodeIds [i][1]);
-        EXPECT_EQ (face [2], expectedTriangleNodeIds [i][2]);
+    for (mk::UInt i = 0; i < triangulation.NumberOfFaces(); ++i)
+    {
+        auto face = triangulation.GetNodeIds(i);
+        EXPECT_EQ(face[0], expectedTriangleNodeIds[i][0]);
+        EXPECT_EQ(face[1], expectedTriangleNodeIds[i][1]);
+        EXPECT_EQ(face[2], expectedTriangleNodeIds[i][2]);
     }
 
-    for (mk::UInt i = 0; i < triangulation.NumberOfFaces(); ++i) {
-        auto faceNodes = triangulation.GetNodes (i);
-        EXPECT_EQ (faceNodes [0].x, triangulation.GetNode (expectedTriangleNodeIds [i][0]).x);
-        EXPECT_EQ (faceNodes [0].y, triangulation.GetNode (expectedTriangleNodeIds [i][0]).y);
+    for (mk::UInt i = 0; i < triangulation.NumberOfFaces(); ++i)
+    {
+        auto faceNodes = triangulation.GetNodes(i);
+        EXPECT_EQ(faceNodes[0].x, triangulation.GetNode(expectedTriangleNodeIds[i][0]).x);
+        EXPECT_EQ(faceNodes[0].y, triangulation.GetNode(expectedTriangleNodeIds[i][0]).y);
 
-        EXPECT_EQ (faceNodes [1].x, triangulation.GetNode (expectedTriangleNodeIds [i][1]).x);
-        EXPECT_EQ (faceNodes [1].y, triangulation.GetNode (expectedTriangleNodeIds [i][1]).y);
+        EXPECT_EQ(faceNodes[1].x, triangulation.GetNode(expectedTriangleNodeIds[i][1]).x);
+        EXPECT_EQ(faceNodes[1].y, triangulation.GetNode(expectedTriangleNodeIds[i][1]).y);
 
-        EXPECT_EQ (faceNodes [2].x, triangulation.GetNode (expectedTriangleNodeIds [i][2]).x);
-        EXPECT_EQ (faceNodes [2].y, triangulation.GetNode (expectedTriangleNodeIds [i][2]).y);
+        EXPECT_EQ(faceNodes[2].x, triangulation.GetNode(expectedTriangleNodeIds[i][2]).x);
+        EXPECT_EQ(faceNodes[2].y, triangulation.GetNode(expectedTriangleNodeIds[i][2]).y);
     }
 
     // Origin of mesh is {0, 0}, with delta {1,1} and there are 3x3 nodes
@@ -72,34 +64,32 @@ TEST(MeshPropertyTests, TriangulationTest)
     std::vector<mk::UInt> expectedElementId{0, 0, 4, 11, 15, 15};
     std::vector<bool> expectedIsInElement{false, true, true, false, false, true};
 
-    for (size_t i = 0; i < pointsToCheck.size (); ++i)
+    for (size_t i = 0; i < pointsToCheck.size(); ++i)
     {
-        mk::UInt faceId = triangulation.FindNearestFace (pointsToCheck [i]);
-        EXPECT_EQ (faceId, expectedElementId[i]);
-        bool isInFace = triangulation.PointIsInElement (pointsToCheck [i], faceId);
-        EXPECT_EQ (isInFace, expectedIsInElement[i]);
+        mk::UInt faceId = triangulation.FindNearestFace(pointsToCheck[i]);
+        EXPECT_EQ(faceId, expectedElementId[i]);
+        bool isInFace = triangulation.PointIsInElement(pointsToCheck[i], faceId);
+        EXPECT_EQ(isInFace, expectedIsInElement[i]);
     }
-
 }
 
 TEST(MeshPropertyTests, TriangulationFailureTest)
 {
     // Not enough points
-    EXPECT_THROW (mk::MeshTriangulation triangulation(std::vector<double>{0.0, 1.0}, std::vector<double>{0.0, 1.0}, mk::Projection::cartesian), mk::ConstraintError);
+    EXPECT_THROW(mk::MeshTriangulation triangulation(std::vector<double>{0.0, 1.0}, std::vector<double>{0.0, 1.0}, mk::Projection::cartesian), mk::ConstraintError);
     // x- and y-points not same size
-    EXPECT_THROW (mk::MeshTriangulation triangulation(std::vector<double>{0.0, 1.0, 2.0}, std::vector<double>{0.0, 1.0}, mk::Projection::cartesian), mk::ConstraintError);
+    EXPECT_THROW(mk::MeshTriangulation triangulation(std::vector<double>{0.0, 1.0, 2.0}, std::vector<double>{0.0, 1.0}, mk::Projection::cartesian), mk::ConstraintError);
 
     std::vector<double> xValues{0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0};
     std::vector<double> yValues{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 3.0, 3.0, 3.0, 3.0};
     mk::MeshTriangulation triangulation(xValues, yValues, mk::Projection::cartesian);
 
     // Invalid point passed to PointIsInElement
-    EXPECT_THROW (triangulation.PointIsInElement({mk::constants::missing::doubleValue, mk::constants::missing::doubleValue}, 1), mk::ConstraintError);
+    EXPECT_THROW(triangulation.PointIsInElement({mk::constants::missing::doubleValue, mk::constants::missing::doubleValue}, 1), mk::ConstraintError);
     // Face id is null
-    EXPECT_THROW (triangulation.PointIsInElement({0.25, 0.25}, mk::constants::missing::uintValue), mk::ConstraintError);
+    EXPECT_THROW(triangulation.PointIsInElement({0.25, 0.25}, mk::constants::missing::uintValue), mk::ConstraintError);
     // Face id is out of range
-    EXPECT_THROW (triangulation.PointIsInElement({0.25, 0.25}, 100), mk::ConstraintError);
-
+    EXPECT_THROW(triangulation.PointIsInElement({0.25, 0.25}, 100), mk::ConstraintError);
 }
 
 TEST(MeshPropertyTests, SimpleSampleInterpolationTest)
