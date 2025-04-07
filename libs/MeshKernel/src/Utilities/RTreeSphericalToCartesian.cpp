52cd194b24827ad882fc96b7dfe44b995494169f
BillSenior <bill.senior@deltares.nl>
2025-04-07 09:52:07 +0200

    GRIDEDIT-1793 Small refactor. Made small function converting spherical coords to Cartesian coords

--- libs/MeshKernel/src/Utilities/RTreeSphericalToCartesian.cpp
+++ libs/MeshKernel/src/Utilities/RTreeSphericalToCartesian.cpp
@@ -2,6 +2,14 @@
 
 #include <cmath>
 
+meshkernel::RTreeSphericalToCartesian::Point2D meshkernel::RTreeSphericalToCartesian::convert(const Point& node) const
+{
+    double x = constants::geometric::earth_radius * std::cos((node.y + 180.0) * constants::conversion::degToRad) * std::cos((node.x + 180.0) * constants::conversion::degToRad);
+    double y = constants::geometric::earth_radius * std::cos((node.y + 180.0) * constants::conversion::degToRad) * std::sin((node.x + 180.0) * constants::conversion::degToRad);
+    double z = constants::geometric::earth_radius * std::sin((node.y + 180.0) * constants::conversion::degToRad);
+    return {x, y, z};
+}
+
 void meshkernel::RTreeSphericalToCartesian::Search(Point const& node, double searchRadiusSquared, bool findNearest)
 {
     if (Empty())
@@ -12,14 +20,10 @@ void meshkernel::RTreeSphericalToCartesian::Search(Point const& node, double sea
     m_queryCache.reserve(m_queryVectorCapacity);
     m_queryCache.clear();
 
-    double x = constants::geometric::earth_radius * std::cos(node.y * constants::conversion::degToRad) * std::cos(node.x * constants::conversion::degToRad);
-    double y = constants::geometric::earth_radius * std::cos(node.y * constants::conversion::degToRad) * std::sin(node.x * constants::conversion::degToRad);
-    double z = constants::geometric::earth_radius * std::sin(node.y * constants::conversion::degToRad);
-
-    const Point2D nodeSought = Point2D(x, y, z);
+    const Point2D nodeSought = convert(node);
     const auto searchRadius = std::sqrt(searchRadiusSquared);
-    Box2D const box(Point2D(x - searchRadius, y - searchRadius, z - searchRadius),
-                    Point2D(x + searchRadius, y + searchRadius, z + searchRadius));
+    Box2D const box(Point2D(bg::get<0>(nodeSought) - searchRadius, bg::get<1>(nodeSought) - searchRadius, bg::get<2>(nodeSought) - searchRadius),
+                    Point2D(bg::get<0>(nodeSought) + searchRadius, bg::get<1>(nodeSought) + searchRadius, bg::get<2>(nodeSought) + searchRadius));
 
     auto pointIsNearby = [&nodeSought, &searchRadiusSquared](Value2D const& v)
     { return bg::comparable_distance(v.first, nodeSought) <= searchRadiusSquared; };
@@ -70,11 +74,7 @@ void meshkernel::RTreeSphericalToCartesian::SearchNearestPoint(Point const& node
     m_queryCache.reserve(m_queryVectorCapacity);
     m_queryCache.clear();
 
-    double x = constants::geometric::earth_radius * std::cos(node.y * constants::conversion::degToRad) * std::cos(node.x * constants::conversion::degToRad);
-    double y = constants::geometric::earth_radius * std::cos(node.y * constants::conversion::degToRad) * std::sin(node.x * constants::conversion::degToRad);
-    double z = constants::geometric::earth_radius * std::sin(node.y * constants::conversion::degToRad);
-
-    const Point2D nodeSought = Point2D(x, y, z);
+    const Point2D nodeSought = convert(node);
     m_rtree2D.query(bgi::nearest(nodeSought, 1), std::back_inserter(m_queryCache));
 
     if (!m_queryCache.empty())
