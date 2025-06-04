511c3c029fb7161524d60b7fdf83792002eb1a35
BillSenior <bill.senior@deltares.nl>
2025-06-04 17:49:29 +0200

    GRIDEDIT-1699 Fixed clang formatting warning

--- libs/MeshKernel/include/MeshKernel/Polygon.hpp
+++ libs/MeshKernel/include/MeshKernel/Polygon.hpp
@@ -142,7 +142,6 @@ namespace meshkernel
         std::tuple<double, double> SegmentLengthExtrema() const;
 
     private:
-
         /// @brief Refines the segment between two polygon nodes.
         ///
         /// Starting with the node specified by the iterator up to, but not including, the next node.
