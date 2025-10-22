6fc74ed278155b03ac753a91f351d0ef86068f30
Scott Willcox <Scott.Willcox@deltares.nl>
2025-10-22 12:13:34 +0200

    GRIDEDIT-1971 added cmake policy version number for compatibility

--- scripts/install_netcdf_static.ps1
+++ scripts/install_netcdf_static.ps1
@@ -244,6 +244,7 @@ Function Invoke-BuildAndInstall {
     $ConfigArgs += ('-B {0}' -f $BuildDir)
     $ConfigArgs += ('-DCMAKE_INSTALL_PREFIX={0}' -f $InstallDir)
     $ConfigArgs += ('-DCMAKE_BUILD_TYPE={0}' -f $BuildType)
+    $ConfigArgs += ('-DCMAKE_POLICY_VERSION_MINIMUM=3.5')
     $ConfigArgs += $Options
     $Configure = ('cmake {0}' -f ($ConfigArgs -Join ' '))
     Write-Host $Configure
