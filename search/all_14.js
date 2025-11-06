ee87aab4b5420cfa060797d70e8226487ed9a222
BillSenior <BillSenior@users.noreply.github.com>
2025-11-06 16:22:42 +0000

    deploy: b4cc83618de3f3d319d4c734f2b002627b85f253

--- search/all_14.js
+++ search/all_14.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['walkboundaryfromnode_0',['WalkBoundaryFromNode',['../classmeshkernel_1_1_mesh2_d.html#a9ccf24cf23136393e36d0fd07bbbfff2',1,'meshkernel::Mesh2D']]],
-  ['what_1',['what',['../classmeshkernel_1_1_mesh_kernel_error.html#a3662d9578c24a326b69fa4fca12fdee1',1,'meshkernel::MeshKernelError']]],
-  ['width_2',['Width',['../classmeshkernel_1_1_bounding_box.html#a582e92518f7ad17457176eb147ba9e42',1,'meshkernel::BoundingBox']]]
+  ['what_0',['what',['../classmeshkernel_1_1_mesh_kernel_error.html#a3662d9578c24a326b69fa4fca12fdee1',1,'meshkernel::MeshKernelError']]],
+  ['width_1',['Width',['../classmeshkernel_1_1_bounding_box.html#a582e92518f7ad17457176eb147ba9e42',1,'meshkernel::BoundingBox']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -5,11 +5,12 @@ var searchData=
   ['deletehangingedges_2',['DeleteHangingEdges',['../classmeshkernel_1_1_mesh2_d.html#af7e137acb6090e974dccfcb585fdf680',1,'meshkernel::Mesh2D']]],
   ['deleteinvalidnodesandedges_3',['DeleteInvalidNodesAndEdges',['../classmeshkernel_1_1_mesh.html#adadf16bf95337fec05384a2dc07798fe',1,'meshkernel::Mesh']]],
   ['deletemesh_4',['DeleteMesh',['../classmeshkernel_1_1_mesh2_d.html#ab9aff255bad361448c527200cfe16e09',1,'meshkernel::Mesh2D']]],
-  ['deletenode_5',['DeleteNode',['../classmeshkernel_1_1_mesh.html#a755064d05033b73f98f6d2543775c471',1,'meshkernel::Mesh']]],
-  ['deletesmallflowedges_6',['DeleteSmallFlowEdges',['../classmeshkernel_1_1_mesh2_d.html#a2321d2206bfadce36a204e90aaab8275',1,'meshkernel::Mesh2D']]],
-  ['deletesmalltrianglesatboundaries_7',['DeleteSmallTrianglesAtBoundaries',['../classmeshkernel_1_1_mesh2_d.html#a76811ff4607395880ad6de6b0e9da326',1,'meshkernel::Mesh2D']]],
-  ['delta_8',['Delta',['../classmeshkernel_1_1_bounding_box.html#a8b1c03b754fcf2e2583efff4a891c69c',1,'meshkernel::BoundingBox']]],
-  ['discretizetransfinite_9',['DiscretizeTransfinite',['../namespacemeshkernel.html#a7789e03bf645903c9a9284c93e74efd9',1,'meshkernel']]],
-  ['distancefromline_10',['DistanceFromLine',['../namespacemeshkernel.html#ada8947b2dced00532f67cc6331dfe18d',1,'meshkernel']]],
-  ['dot_11',['dot',['../namespacemeshkernel.html#a850116a9b7dcd6ac9de408c933bb5e08',1,'meshkernel::dot(const Point &amp;p1, const Point &amp;p2)'],['../namespacemeshkernel.html#a46c4949453f4ccd0a0665a22b5eefe1e',1,'meshkernel::dot(const Point &amp;p, const Vector &amp;v)'],['../namespacemeshkernel.html#acf5ffaf3fedceee72e69d15769156f62',1,'meshkernel::dot(const Vector &amp;v, const Point &amp;p)'],['../namespacemeshkernel.html#aec0e604161b010fc791f1c101e15e929',1,'meshkernel::dot(const Vector &amp;v1, const Vector &amp;v2)']]]
+  ['deletemeshfacesinpolygon_5',['DeleteMeshFacesInPolygon',['../classmeshkernel_1_1_mesh2_d.html#ace13154fdc98be5905f292d69e819f6c',1,'meshkernel::Mesh2D']]],
+  ['deletenode_6',['DeleteNode',['../classmeshkernel_1_1_mesh.html#a755064d05033b73f98f6d2543775c471',1,'meshkernel::Mesh']]],
+  ['deletesmallflowedges_7',['DeleteSmallFlowEdges',['../classmeshkernel_1_1_mesh2_d.html#a2321d2206bfadce36a204e90aaab8275',1,'meshkernel::Mesh2D']]],
+  ['deletesmalltrianglesatboundaries_8',['DeleteSmallTrianglesAtBoundaries',['../classmeshkernel_1_1_mesh2_d.html#a76811ff4607395880ad6de6b0e9da326',1,'meshkernel::Mesh2D']]],
+  ['delta_9',['Delta',['../classmeshkernel_1_1_bounding_box.html#a8b1c03b754fcf2e2583efff4a891c69c',1,'meshkernel::BoundingBox']]],
+  ['discretizetransfinite_10',['DiscretizeTransfinite',['../namespacemeshkernel.html#a7789e03bf645903c9a9284c93e74efd9',1,'meshkernel']]],
+  ['distancefromline_11',['DistanceFromLine',['../namespacemeshkernel.html#ada8947b2dced00532f67cc6331dfe18d',1,'meshkernel']]],
+  ['dot_12',['dot',['../namespacemeshkernel.html#a850116a9b7dcd6ac9de408c933bb5e08',1,'meshkernel::dot(const Point &amp;p1, const Point &amp;p2)'],['../namespacemeshkernel.html#a46c4949453f4ccd0a0665a22b5eefe1e',1,'meshkernel::dot(const Point &amp;p, const Vector &amp;v)'],['../namespacemeshkernel.html#acf5ffaf3fedceee72e69d15769156f62',1,'meshkernel::dot(const Vector &amp;v, const Point &amp;p)'],['../namespacemeshkernel.html#aec0e604161b010fc791f1c101e15e929',1,'meshkernel::dot(const Vector &amp;v1, const Vector &amp;v2)']]]
 ];
